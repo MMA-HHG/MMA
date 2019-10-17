1058e7c9186b43ed74f01f8a535204a2bea27df5
Jan Vabek <vabekjan@gmail.com>
2019-10-17 17:18:28 +0200

    load fields

--- Hankel.py
+++ Hankel.py
@@ -3,6 +3,7 @@ import numpy as np
 import struct
 import array
 import os
+#import string
 
 def myfun():
   print("abc")
@@ -58,6 +59,41 @@ for k1 in range(Nomega): y[k1] = xxx[2*k1]+1j*xxx[2*k1+1];
 
 
 print(y[Nomega-1])
+
+
+folders = []
+
+# r=root, d=directories, f = files
+#for r, d, f in os.walk("."):
+#    for folder in d:
+#        folders.append(os.path.join(r, folder))
+
+#for f in folders:
+#    print(f)
+
+
+dirs=os.listdir("35c/TDSEs2")
+#print(dirs)
+print(dirs[0])
+
+
+#tstr='xx'
+tstr=str(1)
+tstr='35c/z_000501_'+tstr.zfill(6)
+
+print(tstr)
+
+Nfiles=2048;
+FField_r=np.empty([Nomega,Nfiles], dtype=np.cdouble)
+for k1 in range(Nfiles):
+  fold=str(k1+1); fold = '35c/z_000501_'+fold.zfill(6)+'/'
+  FSourceTermPath = fold+'FSourceTerm.bin'
+  file1=open(FSourceTermPath,"rb")
+  dum = array.array('d');
+  dum.fromfile(file1,2*Nomega);
+  for k2 in range(Nomega): FField_r[k2,k1] = dum[2*k2]+1j*dum[2*k2+1];
+
+
 
 #print(complex(xxx[1],z[1]))
 
