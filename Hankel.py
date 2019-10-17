e13aaceb0af0c5047955078cca84815165053185
Jan Vabek <vabekjan@gmail.com>
2019-10-17 16:41:59 +0200

    learning python

--- Hankel.py
+++ Hankel.py
@@ -1,1 +1,73 @@
+from scipy import special
+import numpy as np
+import struct
+import array
+import os
+
+def myfun():
+  print("abc")
+
+
 print("Hello world")
+
+myfun()
+
+a = special.jn(0,1)
+
+print(a)
+
+file1=open("35c/fields/rgrid.dat","r")
+#if file1.mode == "r":
+lines = file1.readlines()
+k1=0; z=[]
+for xxx in lines: y = xxx.split(); z.append(float(y[1])); k1=k1+1
+Nr = k1; z=np.asarray(z);  
+file1.close()  
+
+
+print(z)
+#for k1 in range(Nr): print(z[k1])
+
+
+file1=open("GridDimensionsForBinaries.dat","r")
+lines = file1.readlines()
+file1.close();
+Nomega = int(lines[1]);
+print(Nomega)
+
+
+
+file1=open("omegagrid.bin","rb")
+xxx = array.array('d');
+xxx.fromfile(file1,Nomega);
+file1.close();
+#xxx = struct.unpack('d',file1.read(8))
+#print(xxx)
+
+file1=open("FSourceTerm.bin","rb")
+xxx = array.array('d');
+xxx.fromfile(file1,2*Nomega);
+#xxx = struct.unpack('d',file1.read(8))
+file1.close();
+#print(xxx)
+#for k1 in range(Nomega):
+#  print(z[k1])
+
+y=np.empty([Nomega], dtype=np.cdouble)
+for k1 in range(Nomega): y[k1] = xxx[2*k1]+1j*xxx[2*k1+1];
+
+
+print(y[Nomega-1])
+
+#print(complex(xxx[1],z[1]))
+
+
+
+
+
+
+
+
+
+
+
