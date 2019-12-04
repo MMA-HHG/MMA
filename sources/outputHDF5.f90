d13f3441d369795e3cfb8122c2dbe8a67f35ddee
Jan Vabek <vabekjan@gmail.com>
2019-12-04 20:54:48 +0100

    preparation for MPI-HDF5 2

--- sources/outputHDF5.f90
+++ sources/outputHDF5.f90
@@ -33,59 +33,56 @@ CONTAINS
 	INTEGER(HID_T) :: dset_id       ! Dataset identifier 
 	INTEGER(HID_T) :: dataspace     ! Dataspace identifier 
 	INTEGER(HID_T) :: memspace      ! Memory dataspace identifier 
-	INTEGER(HID_T) :: crp_list      ! Dataset creation property identifier 
+	INTEGER(HID_T) :: h5parameters      ! Dataset creation property identifier 
 
     !!! in the first run, create extendible dataset and fill data
 	field_dimensions = 3;
 	IF (!FIRST RUN)
 
-	!
-	!Initialize FORTRAN predifined datatypes
-	!
+
+	!Initialize HDF5
 	CALL h5open_f(error) 
 
+	!define parameters of HDF5 workflow for MPI-access
+	CALL h5pcreate_f(H5P_DATASET_CREATE_F, h5parameters, error) ! create access parameters
+        CALL h5pset_dxpl_mpio_f(h5parameters, H5FD_MPIO_COLLECTIVE_F, error) ! allow MPI access (should it be here?)
 
-	!Open file for reading and writing ! ? WHAT SHOULD I USE FOR PARALLEL
-	CALL h5fopen_f(filename, H5F_ACC_RDWR file_id, error)
-
-
+	!Open collectivelly the file
+	CALL h5fopen_f(filename, H5F_ACC_RDWR, file_id, error, access_prp = h5parameters ) ! open file collectivelly
+	CALL h5pclose(h5parameters) ! parameters were used for MPI open, close them
+	
 
 	!Create the dataspace with unlimited dimension in z. ! again, what should I use for parallel access?
 	maxdims = (/H5S_UNLIMITED_F, dim_r, dim_t/)
-	CALL h5screate_simple_f(field_dimensions, dims, dataspace, error, maxdims) 
+	dims = (/1,dim_r,dim_t/)
+	CALL h5screate_simple_f(field_dimensions, dims, dataspace, error, maxdims) ! create dataspace, the dataspace is its identifier !!!(global dataspace for all the data)
+	dims = (1, dim_r_end(num_proc)-dim_r_start(num_proc), dim_t) ! dimension of my field
+	CALL h5screate_simple_f(field_dimensions, dims, dataspacelocal, error, maxdims) ! create dataspace, the dataspace is its identifier !!!(local dataspace for local data)
 
 
-	!Modify dataset creation properties, i.e. enable chunking
+	! we create the dataset collectivelly
+	CALL h5dcreate_f(file_id, dsetname, H5T_NATIVE_FLOAT, filespace, dset_id, error) ! according to smilei, propabably optional args 
 
-	CALL h5pcreate_f(H5P_DATASET_CREATE_F, crp_list, error)
-	CALL h5pset_chunk_f(crp_list, field_dimensions, dimsc, error)
-
-        CALL h5pset_dxpl_mpio_f(plist_id, H5FD_MPIO_INDEPENDENT_F, error) ! allow MPI access - should it be here?
-
-	!Create dataset
-	CALL h5dcreate_f(file_id, dsetname, H5T_NATIVE_FLOAT, dataspace, dset_id, error, crp_list)
-	
-
+	!we use hyperslab to assign part of the global dataset
 	!chunk data for each worker
-	offset = (/1,dim_r_start(num_proc),0/)
+	offset = (/0,dim_r_start(num_proc),0/)
 	ccount = (/1, dim_r_end(num_proc) - dim_r_start(num_proc) , dim_t/)
-	CALL h5sselect_hyperslab_f(dataspace, H5S_SELECT_SET_F, offset, count, error)
+	CALL h5sselect_hyperslab_f(filespace, H5S_SELECT_SET_F, offset, count, error) ! we should have access to its part of the dataset for each worker
 
-	CALL h5sclose_f(dataspace, error) ! now, all prerequisities for writing are done
-
-
-	! Now I use hyperslab to write chunked data
-	! reallocate data
-	do k1 = dim_r_start(num_proc),dim_r_end(num_proc)
-		do k2 = 1, dim_t
-			data_append(k1,k2) = REAL(e(k2,k1))
-		enddo
-	enddo
+	!Finally, write data
+	CALL h5pcreate_f(H5P_DATASET_XFER_F, h5parameters, error)
+	CALL h5pset_dxpl_mpio_f(h5parameters, H5FD_MPIO_COLLECTIVE_F, error) ! collective writting
+!	CALL h5pset_chunk_f(crp_list, field_dimensions, dimsc, error) ???????????? Do we need chunk it?
+	CALL h5dwrite_vl_f(dset_id , H5T_NATIVE_FLOAT, Fields, dimsfi, error, memspace,file_space_id=filespace,xfer_prp = h5parameters)! data are written !!!( probably variable length)
+	CALL h5pclose(h5parameters) ! parameters were used for MPI open, close them
 	
- 	CALL H5dwrite_f(dset_id, H5T_NATIVE_FLOAT, data_append, data_dims, error, memspace, dataspace)
+	DO k1=1,dim_t	
+	DO k2=1,dim_t
+		Fields(1,k1,k2) = REAL(e(k1,k2));
+	ENDDO
+	ENDDO
 
 
-	!close the file
 
 	
 	CALL h5close_f(error) ! close the HDF5 workspace
