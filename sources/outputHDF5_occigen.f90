ce6148832beac6d163534611976a38d0ced7e1d2
Jan Vabek <vabekjan@gmail.com>
2020-01-17 14:29:49 +0100

    STABLE VERSION OF SINGLE WRITER: The proc 0 now writes an example test.h5 file. Next task is to try to fill the data by its appropriate example data, i.e. the test file will contain its hyperlab part.

--- sources/outputHDF5_occigen.f90
+++ sources/outputHDF5_occigen.f90
@@ -106,7 +106,7 @@ CONTAINS
 	!
 	! Initialize FORTRAN interface.
 	CALL h5open_f(error)
-	CALL h5fcreate_f(filename2, H5F_ACC_TRUNC_F, file_id, error) ! create test file
+	CALL h5fcreate_f(filename2, H5F_ACC_TRUNC_F, file_id, error) ! create test file 
 	! CALL h5fopen_f (filename, H5F_ACC_RDWR_F, file_id, error) ! Open an existing file.
     CALL h5screate_simple_f(2, data_dims, dataspace, error) ! Create the dataspace.
 	CALL h5dcreate_f(file_id, dsetname2, H5T_NATIVE_INTEGER, dataspace, dset_id, error) ! create the dataset
