1db5f3957098b478af8e5f7dc445615a94f04b3b
Jan Vabek <vabekjan@gmail.com>
2020-01-17 17:06:02 +0100

    debug9

--- sources/outputHDF5_occigen.f90
+++ sources/outputHDF5_occigen.f90
@@ -133,11 +133,11 @@ CONTAINS
 	CALL h5open_f(error) 
     
 	!define parameters of HDF5 workflow for MPI-access
-	CALL h5pcreate_f(H5P_DATASET_CREATE_F, h5parameters, error) ! create access parameters
-    CALL h5pset_fapl_mpio_f(h5parameters, comm,info, error) ! allow MPI access (should it be here?)
+	CALL h5pcreate_f(H5P_FILE_ACCESS_F, h5parameters, error) ! create access parameters
+    CALL h5pset_fapl_mpio_f(h5parameters, comm, info, error) ! allow MPI access (should it be here?)
 
 	!Open collectivelly the file
-	CALL h5fcreate_f(filename2, H5F_ACC_TRUNC_F, file_id, error, access_prp = h5parameters) ! we again first test creating the file collectivelly
+	! CALL h5fcreate_f(filename2, H5F_ACC_TRUNC_F, file_id, error, access_prp = h5parameters) ! we again first test creating the file collectivelly
 	! CALL h5fcreate_f(filename2, H5F_ACC_TRUNC_F, file_id, error)  ! single - version
 	! CALL h5fopen_f(filename, H5F_ACC_RDWR_F, file_id, error, access_prp = h5parameters ) ! open file collectivelly
 
