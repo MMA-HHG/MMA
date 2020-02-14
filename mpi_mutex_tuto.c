589e588b0a010dc095e276e999ca175a48fbe1ad
Jan Vabek <vabekjan@gmail.com>
2020-02-14 17:47:33 +0100

    db2

--- mpi_mutex_tuto.c
+++ mpi_mutex_tuto.c
@@ -1,7 +1,5 @@
 /*
-We test MPI-windows here
-
-It implements the tuto for numerical computing pi. origin "Using MPI-2, chapter 2.3.2"
+We test MPI3-mutex here
 */
 #include<time.h> 
 #include<stdio.h>
@@ -10,72 +8,55 @@ It implements the tuto for numerical computing pi. origin "Using MPI-2, chapter 
 #include<malloc.h>
 #include<math.h>
 #include "hdf5.h"
-#include "util.h"
+// #include "util.h"
 
 
+int MPE_MUTEX_KEYVAL;
 
-/* Compute pi by numerical integration, RMA version */
+/* see desrption pg 198 MPI-2 */
+
+// github.com/shawfdong/ams250/blob/master/examples/mpi/Using_MPI/advanced/nxtval-test.c
+
 int main(int argc, char *argv[])
 {
-    //  simulate the user inputs
-    int user_inputs[10] = {15, 10, 5, 25, 10, 15, 50, 60, 0, 15}; 
-    int iterator = 0;
 
-int n, myid, numprocs, i;
-double PI25DT = 3.141592653589793238462643;
-double mypi, pi, h, sum, x;
-MPI_Win nwin, piwin;
+int myid, numprocs, i;
+
+
+// int MPE_COUNTER_KEYVAL; // how to fix this?
+
+MPI_Win mutex_win; // this is memory window for the counter
 MPI_Init(&argc,&argv);
 MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-if (myid == 0) {
-MPI_Win_create(&n, sizeof(int), 1, MPI_INFO_NULL,
-MPI_COMM_WORLD, &nwin);
-MPI_Win_create(&pi, sizeof(double), 1, MPI_INFO_NULL,
-MPI_COMM_WORLD, &piwin);
-}
-else {
-MPI_Win_create(MPI_BOTTOM, 0, 1, MPI_INFO_NULL,
-MPI_COMM_WORLD, &nwin);
-MPI_Win_create(MPI_BOTTOM, 0, 1, MPI_INFO_NULL,
-MPI_COMM_WORLD, &piwin);
-}
 
-MPI_Win_fence(0, nwin);
-while (1) {
-if (myid == 0) {
-printf("Enter the number of intervals: (0 quits) \n");
-// fflush(stdout);
-// scanf("%d",&n);
-n = user_inputs[iterator];
-pi = 0.0;
-}
-MPI_Win_fence(0, nwin);
-if (myid != 0)
-MPI_Get(&n, 1, MPI_INT, 0, 0, 1, MPI_INT, nwin);
-MPI_Win_fence(0, nwin);
-if (n == 0)
-break;
-else {
-h = 1.0 / (double) n;
-sum = 0.0;
-for (i = myid + 1; i <= n; i += numprocs) {
-x = h * ((double)i - 0.5);
-sum += (4.0 / (1.0 + x*x));
-}
-mypi = h * sum;
-MPI_Win_fence( 0, piwin);
-MPI_Accumulate(&mypi, 1, MPI_DOUBLE, 0, 0, 1, MPI_DOUBLE,
-MPI_SUM, piwin);
-MPI_Win_fence(0, piwin);
-if (myid == 0)
-printf("pi is approximately %lf, Error is %lf \n", pi, fabs(pi - PI25DT));
-}
-++iterator;
-}
-MPI_Win_free(&nwin);
-MPI_Win_free(&piwin);
+printf("Non-mutexed outputs are here\n");
+MPI_Barrier( MPI_COMM_WORLD )
+
+printf("1 I am node %d of %d\n", myid, numprocs);
+sleep(2);
+printf("2 I am node %d of %d\n", myid, numprocs);
+sleep(2);
+printf("3 I am node %d of %d\n", myid, numprocs);
+
+
+printf("Mutexed outputs are here\n");
+MPI_Barrier( MPI_COMM_WORLD )
+
+MPE_MUTEX_KEYVAL = MPE_Counter_create(MPI_COMM_WORLD,1, &mutex_win );
+
+int MPE_Mutex_acquire(mutex_win, 0, MPE_MUTEX_KEYVAL) {
+
+printf("1 I am node %d of %d\n", myid, numprocs);
+sleep(2);
+printf("2 I am node %d of %d\n", myid, numprocs);
+sleep(2);
+printf("3 I am node %d of %d\n", myid, numprocs);
+int MPE_Mutex_release(mutex_win, 0, MPE_MUTEX_KEYVAL)
+
+
+// window should be deleted
+
 MPI_Finalize();
 return 0;
 }
-
