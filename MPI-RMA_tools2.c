53d690f5d4858335ca2f30c48dc9571bcc88a320
Jan Vabek <vabekjan@gmail.com>
2020-02-14 15:32:57 +0100

    split files

--- MPI-RMA_tools2.c
+++ MPI-RMA_tools2.c
@@ -8,24 +8,62 @@
 #include "mpi.h"
 
 
+// !!!!!!!!!!! see: https://www.mcs.anl.gov/research/projects/mpi/usingmpi/examples-advmpi/rma2/
 
-int MPE_Counter_nxtval(MPI_Win counterWin, int counterNum, int *value) // MPI-3 version 
+// extern int MPE_MUTEX_KEYVAL;
+
+// extern int MPE_COUNTER_KEYVAL;
+static int MPE_COUNTER_KEYVAL = MPI_KEYVAL_INVALID;
+
+// void MPE_setKeyval(const int keyval) // taken from https://gitlab.isti.com/bbaker/libcps/blob/bc7be134f27f3b2828c2016cd69c4c925958fbea/nxtval.c
+// {
+//     MPE_COUNTER_KEYVAL = keyval;   
+//     return;
+// }
+
+extern int MPEi_CounterFree(MPI_Win counter_win, int keyval, void *attr_val, void *extra_state);
+
+
+void MPE_Counter_create(MPI_Comm comm, int num, MPI_Win *counter_win) // MPI-3 version
 {
-const int one = 1;
-int lrank, flag, size, *attrval;
-MPI_Aint lidx;
-/* Compute the location of the counter */
-MPI_Win_get_attr(counterWin, MPE_COUNTER_KEYVAL, &attrval,&flag);
-if (!flag) return -1; /* Error: counterWin not correctly setup */
-size = (MPI_Aint)attrval; /* We stored the integer as a pointer */
-lrank = counterNum % size;
-lidx = counterNum / size;
-/* Update and return the counter */
-MPI_Win_lock(MPI_LOCK_SHARED, 0, lrank, counterWin);
-MPI_Fetch_and_op(&one, value, MPI_INT,lrank, lidx, MPI_SUM, counterWin);
-MPI_Win_unlock(lrank, counterWin);
-return 0;
+int size, rank, lnum, lleft, i, *counterMem=0;
+MPI_Aint counterSize;
+MPI_Comm_rank(comm, &rank);
+MPI_Comm_size(comm, &size);
+lnum = num / size;
+lleft = num % size;
+if (rank < lleft) lnum++;
+counterSize = lnum * sizeof(int);
+if (counterSize > 0) {
+MPI_Alloc_mem(counterSize, MPI_INFO_NULL, &counterMem);
+for (i=0; i<lnum; i++) counterMem[i] = 0;
 }
+/* By using MPI_Alloc_mem first, we ensure that the initial
+value of the counters are zero. See text */
+MPI_Win_create(counterMem, counterSize, sizeof(int),MPI_INFO_NULL, comm, counter_win);
+/* Create key if necessary and store the number of counters */
+if (MPE_COUNTER_KEYVAL == MPI_KEYVAL_INVALID) {
+MPI_Win_create_keyval(MPI_WIN_NULL_COPY_FN, MPEi_CounterFree, &MPE_COUNTER_KEYVAL, NULL);
+}
+MPI_Win_set_attr(*counter_win, MPE_COUNTER_KEYVAL,
+(void*)(MPI_Aint)num);
+}
+
+
+int MPEi_CounterFree(MPI_Win counter_win, int keyval, void *attr_val, void *extra_state)
+{
+int counter_flag, *counterMem;
+MPI_Win_get_attr(counter_win, MPI_WIN_BASE,&counterMem, &counter_flag);
+/* Free the memory used by the counter */
+if (counter_flag && counterMem)
+MPI_Free_mem(counterMem);
+return MPI_SUCCESS;
+}
+
+
+
+
+
 
 
 // void MPE_Counter_create( MPI_Comm old_comm, MPI_Win *counter_win )
