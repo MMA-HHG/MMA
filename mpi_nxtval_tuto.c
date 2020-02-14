b95e5e9043dadda10951b5495499728ba48cedb0
Jan Vabek <vabekjan@gmail.com>
2020-02-14 13:44:37 +0100

    db16

--- mpi_nxtval_tuto.c
+++ mpi_nxtval_tuto.c
@@ -37,6 +37,8 @@ MPE_Counter_create(MPI_COMM_WORLD, &counter_win );
 
 printf("I am node %d of %d and my counter value before addition is %d \n", myid, numprocs, counter_value);
 
+MPI_Win_fence(0, counter_win);
+
 MPE_Counter_nxtval( counter_win, &counter_value );
 
 printf("I am node %d of %d and my counter value is %d \n", myid, numprocs, counter_value);
