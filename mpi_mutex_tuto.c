72ee655cd6d41178a10523ca3d777a2f25b2237f
Jan Vabek <vabekjan@gmail.com>
2020-02-14 18:02:50 +0100

    db6

--- mpi_mutex_tuto.c
+++ mpi_mutex_tuto.c
@@ -33,13 +33,14 @@ MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 if (myid == 0) { printf("Non-mutexed outputs are here\n"); fflush(stdout);}
 MPI_Barrier( MPI_COMM_WORLD );
 
-printf("1 I am node %d of %d\n", myid, numprocs);
+printf("1 I am node %d of %d\n", myid, numprocs); fflush(stdout);
 sleep(2);
-printf("2 I am node %d of %d\n", myid, numprocs);
+printf("2 I am node %d of %d\n", myid, numprocs); fflush(stdout);
 sleep(2);
-printf("3 I am node %d of %d\n", myid, numprocs);
+printf("3 I am node %d of %d\n", myid, numprocs); fflush(stdout);
 
 
+MPI_Barrier( MPI_COMM_WORLD );
 if (myid == 0) {printf("Mutexed outputs are here\n"); fflush(stdout);}
 MPI_Barrier( MPI_COMM_WORLD );
 
@@ -47,11 +48,11 @@ MPE_MUTEX_KEYVAL = MPE_Counter_create(MPI_COMM_WORLD,1, &mutex_win );
 
 MPE_Mutex_acquire(mutex_win, 0, MPE_MUTEX_KEYVAL);
 
-printf("1 I am node %d of %d\n", myid, numprocs);
+printf("1 I am node %d of %d\n", myid, numprocs); fflush(stdout);
 sleep(2);
-printf("2 I am node %d of %d\n", myid, numprocs);
+printf("2 I am node %d of %d\n", myid, numprocs); fflush(stdout);
 sleep(2);
-printf("3 I am node %d of %d\n", myid, numprocs);
+printf("3 I am node %d of %d\n", myid, numprocs); fflush(stdout);
 
 MPE_Mutex_release(mutex_win, 0, MPE_MUTEX_KEYVAL);
 
