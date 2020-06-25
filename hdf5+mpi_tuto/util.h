d596c64a4ccc7aac1064a2281545bc1858d413e5
Jan Vabek <vabekjan@gmail.com>
2020-06-25 13:54:08 +0200

    update util

--- hdf5+mpi_tuto/util.h
+++ hdf5+mpi_tuto/util.h
@@ -116,6 +116,13 @@ void printlimitedFFTW3(FILE *, double *, int, double, double, double);
 void define_analytical(struct Efield_var *, FILE *);
 
 
+// HDF5
+int MPE_Counter_create(MPI_Comm, int, MPI_Win *)
+int MPE_Counter_nxtval(MPI_Win, int, int *, int)
+int MPE_Mutex_acquire(MPI_Win, int, int)
+int MPE_Mutex_release(MPI_Win, int, int)
+
+
 
 
 
