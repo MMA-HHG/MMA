a6effad17c847e2d461f6ec4b37758cdd4fda073
Jan Vabek <vabekjan@gmail.com>
2020-07-13 16:49:39 +0200

    consts by initialisation

---
+++ constants.c
@@ -0,0 +1,19 @@
+#include<math.h>
+#include<malloc.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include"util.h"
+
+
+void Init_constants(void){
+    double pi = acos(-1.0);
+    Ip_HeV = 27.21138602;
+    hbar = 1.054571800e-34;
+    alpha_fine = 1/137.035999139;
+    c_light = 299792458;
+    elcharge = 1.602176565e-19;
+    elmass = 9.10938356e-31;
+    mu0 = 4*pi*1e-7;
+    eps0 = 1/(mu0*c_light^2);
+    r_Bohr = 4*pi*eps0*hbar^2/(elmass*elcharge^2);
+}
