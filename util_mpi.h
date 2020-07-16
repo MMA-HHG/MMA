abf31cd78890eb903e3c8958b5f2f73f5049e2c4
Jan Vabek <vabekjan@gmail.com>
2020-07-16 16:21:47 +0200

    utils separated

---
+++ util_mpi.h
@@ -0,0 +1,4 @@
+int MPE_Counter_create(MPI_Comm, int, MPI_Win *);
+int MPE_Counter_nxtval(MPI_Win, int, int *, int);
+int MPE_Mutex_acquire(MPI_Win, int, int);
+int MPE_Mutex_release(MPI_Win, int, int);
