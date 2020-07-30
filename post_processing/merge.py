52b2b35aa2566d69e4ac948ffafc4877c3baf23a
Jan Vabek <vabekjan@gmail.com>
2020-07-30 15:52:43 +0200

    post-processor done
    The Python script to merge all the inputs is done. There is no cleanup and some on at the instant. Data are stored in the root dirctory, will be improved once everything is coupled.

--- post_processing/merge.py
+++ post_processing/merge.py
@@ -1,21 +1,18 @@
+####################################################################################
+# Jan Vabek - ELI-Beamlines, CELIA, CTU in Prague (FNSPE) (2020)
+#
+# The purpose of this code is to merge outputs generated from 1DTDSE. It uses a general
+# procedure for assigning indices based on keys, it thus can be easily generalised for more
+# dimensions.
+
+
+
+# finding mn from external location
 # https://stackoverflow.com/questions/17198319/how-to-configure-custom-pythonpath-with-vm-and-pycharm
 
-# from scipy import special
-# from scipy import integrate
-# from scipy import interpolate
 import numpy as np
-# import struct
-# import array
 import os
-# import time
-# # import ray
-# # import matlab.engine
-# # import string
-# import multiprocessing as mp
 import math
-# # import joblib
-# # from mpi4py import MPI
-# # import oct2py
 import shutil
 import h5py
 import sys
@@ -23,54 +20,55 @@ import units
 import mynumerics as mn
 import glob
 
-print(mn.IsPowerOf2(4))
 
-files = glob.glob('hdf5_temp_*.h5')
-outfname = "merged.h5"
-available_outputs_list = ['Efield', 'Gabor']
-
-print(files)
+files = glob.glob('hdf5_temp_*.h5') # filter all the single-proc files
+outfname = "results_merged.h5"
+available_outputs_list = ['Efield', 'FEfield', 'SourceTerm', 'FSourceterm', 'FEfieldM2', 'FSourceTermM2', 'PopTot', 'Gabor']
+available_further_data = ['tgrid', 'omegagrid', 'Energy_of_the_ground_state'] # these are in all the files and supposed to be same, e.g. grids
+precision = 'd'
 
 def prepare_ouput_file(f,outf,dset_list):
     joint_rz_shape = (mn.readscalardataset(f,'Nr_orig','N')[0], mn.readscalardataset(f,'Nz_orig','N')[0])
     for dsetname in dset_list:
+
         if (dsetname in available_outputs_list):
             dset = f[dsetname]
             newshape = dset.shape[0:-1] + joint_rz_shape
-            newdset = outf.create_dataset(dsetname, newshape,'d')
+            newdset = outf.create_dataset(dsetname, newshape,precision)
+
+        if (dsetname in available_further_data):
+            f.copy(dsetname,outf)
 
 
-
-
-
-
+def print_ouput_file(f,outf,dset_list):
+    Nr = mn.readscalardataset(f, 'Nr_orig', 'N')[0]
+    Nz = mn.readscalardataset(f, 'Nz_orig', 'N')[0]
+    nsim_loc = mn.readscalardataset(f, 'number_of_local_simulations', 'N')[0]
+    ks_loc = f['keys'][()]
+    for dsetname in dset_list:
+        if (dsetname in available_outputs_list):
+            dset = f[dsetname]
+            ndims = len(dset.shape)
+            newdset = outf[dsetname]
+            data = dset[()]
+            for k1 in range(nsim_loc):
+                kr, kz = mn.n1n2mapping_inv(ks_loc[k1], Nr)
+                # print(kr,kz)
+                if (ndims == 2): # for reals
+                    newdset[:,kr,kz] = dset[:,k1]
+                elif (ndims == 3):  # for complex
+                    newdset[:,:,kr,kz] = dset[:,:,k1]
+                else:
+                    print('warning, dataset with unsupported dimension: ' + dsetname + ', nothing done')# general procedure should be possible
 
 with h5py.File(outfname,'w') as outf:
     firstrun = True;
     for fname in files:
-        print("file")
         with h5py.File(fname,'r') as f:
             dset_list = list(f.keys())
             if firstrun:
                 prepare_ouput_file(f, outf, dset_list)
-                firstrun = False;
-            else:
-                pass
-            nsim_loc = mn.readscalardataset(f,'number_of_local_simulations','N')[0]
-            print(nsim_loc)
-            data = f["Efield"][()]
-            print(data[:,0:nsim_loc])
-            # if first: prepare
-            # else: print
-            print(list(f.keys()))
-            myshape = f["FEfield"].shape
-            print(myshape[0:-1])
-
-
-reflist = ["a", "car", "dog"]
-print("car" in reflist)
-print("a" in reflist)
-print("b" in reflist)
-
-
-# scan datasets and then fill it according to keys and lengths (extend each dataset by 2 (resolve dimensions...))
+                firstrun = False
+            print_ouput_file(f, outf, dset_list)
+            
+print('Done')
