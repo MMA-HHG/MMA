bcc60f5ebfa8c553b5b93c80f358904dbcf5d4a7
Jan Vabek <vabekjan@gmail.com>
2020-11-13 00:49:50 +0100

    module prepared for the inputs

---
+++ sources/default_inputs.f90
@@ -0,0 +1,17 @@
+! It was developed by Jan Vabek
+module default_inputs
+
+implicit none
+private
+public  :: a
+
+INTERFACE a
+    procedure a1
+END INTERFACE
+
+
+
+CONTAINS
+
+
+end module default_inputs
