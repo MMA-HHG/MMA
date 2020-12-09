d1ee0b1457f13e3c3dae1c6c5b1d7eb03cde873d
Jan Vabek <vabekjan@gmail.com>
2020-12-09 11:44:23 +0100

    attempt to use centralised naming structure in HDF5 2/n

--- sources/modules.f90
+++ sources/modules.f90
@@ -19,8 +19,8 @@ MODULE h5namelist
   CHARACTER(*), PARAMETER   ::  log_grpname =         "logs"
   CHARACTER(*), PARAMETER   ::  ionref_grpname =      "ionisation_model"
   CHARACTER(*), PARAMETER   ::  longstep_grpname =    "longstep"
-  CHARACTER(*), PARAMETER   ::  outcont_grpname =     out_grpname//"/"//"code_continuation"
-  CHARACTER(*), PARAMETER   ::  refrindex_grpname =   pre_proc_grpname//"/"//"pre-processed"
+  CHARACTER(*), PARAMETER   ::  outcont_grpname =     out_grpname//"/code_continuation"
+  CHARACTER(*), PARAMETER   ::  refrindex_grpname =   pre_proc_grpname//"/pre-processed"
 END MODULE h5namelist
 
 MODULE longstep_vars
