8251025ccd2a83260fb54e75c8eeca421b150c59
Jan Vabek <vabekjan@gmail.com>
2021-01-14 22:28:56 +0100

    add initial files for the analyses

---
+++ python/coherence_map.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+import time
+# import multiprocessing as mp
+import shutil
+import h5py
+import sys
+import units
+import mynumerics as mn
+
+
+
+#### THE MAIN PROGRAM #####
+
+
+# class NumericalParamsClass:
+#   def __init__(self):
+#     pass
+#
+# NumericalParams.z_medium = ParamFile['inputs/'+'jetpositions'][()]
+
+# test matplot
+
