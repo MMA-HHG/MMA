fc036630b0f68a1e1f7c7cb220e4eb81035f5c12
Jan Vabek <vabekjan@gmail.com>
2021-01-15 21:03:19 +0100

    testing colorbar

--- python/coherence_map.py
+++ python/coherence_map.py
@@ -13,16 +13,28 @@ import matplotlib.pyplot as plt
 # results_path = os.path.join("/mnt", "d", "data", "Discharges") # 'D:\data\Discharges'
 results_path = os.path.join("D:\data", "Discharges")
 
-filename = "results_1.h5"
+filename = "results_7.h5"
 
 file_path = os.path.join(results_path,filename)
 
 with h5py.File(file_path, 'r') as InputArchive:
 
     print(InputArchive['/inputs/laser_wavelength'][()])
+    tgrid = InputArchive['/outputs/tgrid'][:]
+    rgrid = InputArchive['/outputs/rgrid'][:]
+    zgrid = InputArchive['/outputs/zgrid'][:]
+    electron_density_map = InputArchive['/outputs/output_plasma'][:]
+    xxx = electron_density_map[0:3,0,0]
+    yyy = electron_density_map[0, 0:3, 0]
 
-    electron_density_map = 5
     plasma_frequency_map = np.sqrt((units.elcharge**2)/(units.eps0*units.elmass)) * np.sqrt(electron_density_map)
+
+    plt.pcolor(tgrid,rgrid,plasma_frequency_map[:,:,0].T)
+    plt.colorbar()
+    plt.savefig('map.png', dpi = 600)
+    plt.show()
+
+
 
 
 ## refractive index contribs
