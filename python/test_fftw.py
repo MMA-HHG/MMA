720fc541a17fbcf3fb205ceb89c85a98e7c2bc69
Jan Vabek <vabekjan@gmail.com>
2021-01-16 22:02:40 +0100

    testing fft

--- python/test_fftw.py
+++ python/test_fftw.py
@@ -18,6 +18,22 @@ filename = "results_5.h5"
 
 file_path = os.path.join(results_path,filename)
 
+
+def myfft(x,fx):
+    if (len(x)%2==1): x = x[0:-1]
+    N = len(x)
+    dx = x[1]-x[0]
+    Fx = (dx/np.sqrt(2.0*np.pi))*np.conj(np.fft.fft(fx)[0:((N//2)+1)])
+    xi = (np.pi/dx)*np.linspace(0,1,(N//2)+1)
+    return xi, Fx
+
+def imyfft(xi,Fx):
+    dxi = xi[1]-xi[0]
+    Fx = np.append(Fx, np.flip(np.conj(Fx[1:])))
+    fx = ((len(Fx)*dxi)/(np.sqrt(2.0*np.pi)))*np.flip(np.fft.ifft(Fx))
+    x = np.linspace(0,2.0*np.pi/dxi,len(fx))
+    return x, fx
+
 with h5py.File(file_path, 'r') as InputArchive:
 
     print(1e-2*InputArchive['/inputs/laser_wavelength'][()])
@@ -29,48 +45,24 @@ with h5py.File(file_path, 'r') as InputArchive:
     electron_density_map = InputArchive['/outputs/output_plasma'][:]
     Efield = InputArchive['/outputs/output_field'][:]
 
-    plasma_frequency_map = np.sqrt((units.elcharge ** 2) / (units.eps0 * units.elmass)) * np.sqrt(electron_density_map)
+    Etest = Efield[:, 0, 30]
 
+    ogrid, FEtest = myfft(tgrid,Etest)
 
-    # n_plasma_map = np.sqrt(1-(plasma_frequency_map/omega0)**2)
-    #
-    # k_plas_map = 25*(2.0*np.pi/mn.ConvertPhoton(omega0,'omegaSI','lambdaSI'))*(n_plasma_map-1)
-    #
-    # plt.pcolor(tgrid,rgrid,k_plas_map[:,:,0].T)
-    # plt.colorbar()
-    # plt.savefig('kmap.png', dpi = 600)
-    # plt.show()
-    #
-    # plt.pcolor(tgrid,rgrid,abs(np.pi/k_plas_map[:,:,15].T))
-    # plt.colorbar()
-    # plt.savefig('Lcohmap.png', dpi = 600)
-    # plt.show()
-    #
-    # xxx = electron_density_map[0:3,0,0]
-    # yyy = electron_density_map[0, 0:3, 0]
+    plt.plot(ogrid,np.abs(FEtest), linewidth=0.2)
+    plt.savefig('FEfield.png', dpi = 600)
+    plt.show()
 
-    plt.plot(tgrid, Efield[:,0,1],linewidth=0.2)
-    plt.plot(tgrid, Efield[:, 0, 2],linewidth=0.2)
-    plt.plot(tgrid, Efield[:, 0, 30], linewidth=0.2)
+    plt.plot(tgrid, Etest, linewidth=0.2)
     plt.savefig('Efield.png', dpi = 600)
     plt.show()
 
+    tt, FFEtest = imyfft(ogrid,FEtest)
 
-    plt.pcolor(tgrid,rgrid,plasma_frequency_map[:,:,0].T)
-    plt.colorbar()
-    plt.savefig('map_zfix.png', dpi = 600)
+    plt.plot(FFEtest, linewidth=0.2)
+    plt.savefig('FFEfield.png', dpi = 600)
     plt.show()
 
-
-    plt.pcolor(zgrid,tgrid,np.squeeze(plasma_frequency_map[:,0,:]))
-    plt.colorbar()
-    plt.savefig('map_onax.png', dpi = 600)
-    plt.show()
-
-    plt.pcolor(zgrid,rgrid,np.squeeze(plasma_frequency_map[512,:,:]))
-    plt.colorbar()
-    plt.savefig('map_thalf.png', dpi = 600)
-    plt.show()
 
 print('test1')
 
