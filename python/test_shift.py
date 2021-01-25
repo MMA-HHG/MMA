c12afcf9fc221513bf24dcfbf5e7af00f1dfe95f
Jan Vabek <vabekjan@gmail.com>
2021-01-26 00:08:16 +0100

    Test shifting in time domain using fft.

---
+++ python/test_shift.py
@@ -0,0 +1,143 @@
+import numpy as np
+import os
+import time
+# import multiprocessing as mp
+import shutil
+import h5py
+import sys
+sys.path.append('D:\git\python_modules')
+import units
+import mynumerics as mn
+# import mynumerics as mn
+import matplotlib.pyplot as plt
+
+# from skimage.restoration import unwrap_phase
+
+# results_path = os.path.join("/mnt", "d", "data", "Discharges") # 'D:\data\Discharges'
+results_path = os.path.join("D:\data", "Discharges")
+
+filename = "results_1.h5"
+
+file_path = os.path.join(results_path,filename)
+
+
+def myfft(x,fx):
+    if (len(x)%2==1): x = x[0:-1]
+    N = len(x)
+    dx = x[1]-x[0]
+    Fx = (dx/np.sqrt(2.0*np.pi))*np.conj(np.fft.fft(fx)[0:((N//2)+1)])
+    xi = (np.pi/dx)*np.linspace(0,1,(N//2)+1)
+    return xi, Fx
+
+def imyfft(xi,Fx):
+    dxi = xi[1]-xi[0]
+    Fx = np.append(Fx, np.flip(np.conj(Fx[1:])))
+    fx = ((len(Fx)*dxi)/(np.sqrt(2.0*np.pi)))*np.flip(np.fft.ifft(Fx))
+    x = np.linspace(0,2.0*np.pi/dxi,len(fx))
+    return x, fx
+
+def complexify_fft(fx):
+    N = len(fx)
+    fx = np.fft.fft(fx)
+    for k1 in range((N//2)+1,N):
+        fx[k1] = 0.0
+    fx = 2.0*np.fft.ifft(fx)
+    return fx
+
+with h5py.File(file_path, 'r') as InputArchive:
+
+    print(1e-2*InputArchive['/inputs/laser_wavelength'][()])
+    omega0 = mn.ConvertPhoton(1e-2*InputArchive['/inputs/laser_wavelength'][()],'lambdaSI','omegaSI')
+    # omega0 = 2379924242424242.5
+    print(omega0)
+    tgrid = InputArchive['/outputs/tgrid'][:]
+    rgrid = InputArchive['/outputs/rgrid'][:]
+    zgrid = InputArchive['/outputs/zgrid'][:]
+    electron_density_map = InputArchive['/outputs/output_plasma'][:]
+    Efield = InputArchive['/outputs/output_field'][:]
+
+
+
+    Etest = Efield[:,0,10]
+
+    omegauppe = 10.1 * omega0
+
+    ogrid, FEtest = myfft(tgrid,Etest)
+
+    plt.plot(ogrid,np.abs(FEtest), linewidth=0.2)
+    plt.savefig('FEfield.png', dpi = 600)
+    plt.show()
+
+    plt.plot(np.abs(FEtest[75:100]), linewidth=0.2)
+    plt.savefig('FEfield_pts.png', dpi = 600)
+    plt.show()
+
+
+    plt.plot(tgrid, Etest, linewidth=0.2)
+    plt.xlim([-30.e-15,30.e-15])
+    plt.savefig('Efield.png', dpi = 600)
+    plt.show()
+
+    delta_t = 50.0e-15  # 0.01
+    # FEtest_shift = np.exp(1j*np.linspace(0,len(ogrid)-1,len(ogrid))*delta_t)*FEtest
+    FEtest_shift = np.exp(1j*ogrid*delta_t)*FEtest
+
+    tt, FFEtest = imyfft(ogrid,FEtest)
+    FFEtest_shift = imyfft(ogrid, FEtest_shift)[1]
+
+    tt_center = tt - (tt[-1]-tt[0])/2.0
+
+    plt.plot(FFEtest, linewidth=0.2)
+    plt.title('FFE')
+    plt.savefig('FFEfield.png', dpi = 600)
+    plt.show()
+
+    plt.plot(tt_center,FFEtest.real, linewidth=0.2)
+    plt.plot(tt_center,FFEtest_shift.real, linewidth=0.2)
+    plt.title('FFE + shift')
+    plt.savefig('FFEfield_s.png', dpi=600)
+    plt.show()
+
+    print('test1')
+
+    # show envelopes
+    # complexify fields
+    rem_fast_oscillations = np.exp(-1j * omega0 * tt_center)
+    Ecmplxe = rem_fast_oscillations*mn.complexify_fft(FFEtest.real)
+    Ecmplxes = rem_fast_oscillations*mn.complexify_fft(FFEtest_shift.real)
+
+    # the amplitude seems to be wrong
+    plt.plot(tt_center,Ecmplxe.real, linewidth=0.2)
+    plt.plot(tt_center,Ecmplxes.real, linewidth=0.2)
+    plt.title('FFE + shift + envel')
+    plt.savefig('FFEfield_cmplxe.png', dpi=600)
+    plt.show()
+
+
+
+    # Ecmplx = complexify_fft(Etest)
+    #
+    # Eenvelope = np.exp(-1j*omega0*tgrid)*Ecmplx
+    #
+    # plt.plot(tgrid, Ecmplx.real, linewidth=0.2)
+    # plt.savefig('EcmplxR.png', dpi = 600)
+    # plt.show()
+    #
+    # plt.plot(tgrid, Eenvelope.real, linewidth=0.2)
+    # plt.savefig('EenvelR.png', dpi = 600)
+    # plt.show()
+    #
+    # plt.plot(tgrid, np.angle(Eenvelope), linewidth=0.2)
+    # plt.savefig('EenvelAngle.png', dpi = 600)
+    # plt.show()
+    #
+    # plt.plot(tgrid, np.unwrap(np.angle(Eenvelope)), linewidth=0.2)
+    # plt.savefig('EenvelAngleUnwrp.png', dpi = 600)
+    # plt.show()
+    #
+    # plt.plot(tgrid, abs(Ecmplx.real-Etest), linewidth=0.2)
+    # plt.savefig('Error.png', dpi = 600)
+    # plt.show()
+
+
+print('done')
