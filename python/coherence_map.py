b01fa77b27b8be4345fda7e3e2f997378d578d4b
Jan Vabek <vabekjan@gmail.com>
2021-02-03 13:22:03 +0100

    formating the code + tested dPhi/dz on uneven grid

--- python/coherence_map.py
+++ python/coherence_map.py
@@ -22,28 +22,35 @@ os.chdir(results_path)
 files = glob.glob('results_*.h5')
 os.chdir(cwd)
 
+files = ['results_1.h5']
+
 out_h5name = 'analyses.h5'
 
-# sys.exit()
+q = 23 # harmonic of our interest
+t_fix = 0.0 # the time of our interest to inspect e.g. phase
+
+
 
 with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analyses
+# This is the archive, where we store numerical results at the instant. It may
+# be further extended by storing also figure sources etc.
     for fname in files:
+    # Here we loop over all result files in the destiantion folder.
         filename = fname
         
-        # k_sim = re.findall(r'\d+', filename)
-        # k_sim = re.search(r'\d+', filename)
+        # Using regexp to obtain the rusult number as a literal.
         k_sim = re.findall(r'\d+', filename)
-        k_sim = k_sim[0]
+        k_sim = k_sim[0] 
         
+        # Prepare group in OutFile and open the repsective result file
+        grp = OutFile.create_group('results_' + str(k_sim))   
         file_path = os.path.join(results_path,filename)
-        
-        # my inputs
-        k_t = 1024 # the choice of time
-        
-        grp = OutFile.create_group('results_' + str(k_sim))        
-        with h5py.File(file_path, 'r') as InputArchive:
-        
-            ## Load data
+        print('processing:', file_path)             
+        with h5py.File(file_path, 'r') as InputArchive:           
+
+            # =================================================================
+            # Load data
+
         
             print(1e-2*mn.readscalardataset(InputArchive,'/inputs/laser_wavelength','N'))
             omega0 = mn.ConvertPhoton(1e-2*mn.readscalardataset(InputArchive,'/inputs/laser_wavelength','N'),'lambdaSI','omegaSI')
@@ -64,58 +71,80 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             rho0_atm = 1e6 * mn.readscalardataset(InputArchive, '/inputs/calculated/medium_effective_density_of_neutral_molecules','N')
             pressure = InputArchive['/inputs/medium_pressure_in_bar'][()]
             
+            
+            # =================================================================
+            # Process the data
             # plasma_frequency_map = np.sqrt((units.elcharge ** 2) / (units.eps0 * units.elmass)) * np.sqrt(electron_density_map)
-            q=23
+    
+            # ===============================================
+            # Refractive indexes of the IR and XUV
+            # XUV is retrieved from the tables https://henke.lbl.gov/optical_constants/asf.html
+            # See also (2.80) in Attwood: X-Rays and Extreme Ultraviolet Radiation Principles and Applications; 2016
+            # IR is given by the Dalgarno, Kingston; 1960
+            
             f1 = XUV_index.getf('Ar', mn.ConvertPhoton(q*omega0, 'omegaSI', 'eV'))[0]
             nXUV = 1 - pressure*rho0_atm*units.r_electron_classical*(mn.ConvertPhoton(omega0,'omegaSI','lambdaSI')**2)*f1/(2.0*np.pi)
-            VF_XUV = units.c_light/nXUV
+            VF_XUV = units.c_light/nXUV # phase velocity of XUV
             
             nIR = IR_index.getsusc('Ar', mn.ConvertPhoton(omega0,'omegaSI','lambdaSI'))
             nIR = np.sqrt(1.0 + pressure*nIR)
-            VF_IR = units.c_light/nIR
+            VF_IR = units.c_light/nIR # phase velocity of IR
             
-            print(units.c_light,VF_IR,1.0/inverse_GV,VF_XUV, sep='\n')
-            print('delta_t_IR_phase =', -1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VF_IR),'fs')
+            # ===============================================
+            # Compute delays and store them
+
             dset_id = grp.create_dataset('delta_t_IR_phase', data=-1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VF_IR))
-            dset_id.attrs['units'] = np.string_('[fs]')
-            
-            print('delta_t_IR_group =', -1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VG_IR),'fs')
+            dset_id.attrs['units'] = np.string_('[fs]')            
+ 
             dset_id = grp.create_dataset('delta_t_IR_group', data=-1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VG_IR))
             dset_id.attrs['units'] = np.string_('[fs]')
-            
-            print('delta_t_XUV_phase =', -1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VF_XUV),'fs')
+
             dset_id = grp.create_dataset('delta_t_XUV_phase', data=-1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VF_XUV))
             dset_id.attrs['units'] = np.string_('[fs]')
-            
-            print('T0_IR =', 1e15*mn.ConvertPhoton(omega0,'omegaSI','T0SI'),'fs')
+
             dset_id = grp.create_dataset('T0_IR', data=1e15*mn.ConvertPhoton(omega0,'omegaSI','T0SI'))
-            dset_id.attrs['units'] = np.string_('[fs]')
-            
-            print('T0_XUV =', 1e15*mn.ConvertPhoton(q*omega0,'omegaSI','T0SI'),'fs')
+            dset_id.attrs['units'] = np.string_('[fs]')            
+
             dset_id = grp.create_dataset('T0_XUV', data=1e15*mn.ConvertPhoton(q*omega0,'omegaSI','T0SI'))
             dset_id.attrs['units'] = np.string_('[fs]')
+
+            print('characteristic velocities')
+            print(units.c_light,VF_IR,1.0/inverse_GV,VF_XUV, sep='\n')
+            print('delta_t_IR_phase =', -1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VF_IR),'fs')      
+            print('delta_t_IR_group =', -1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VG_IR),'fs')            
+            print('delta_t_XUV_phase =', -1e15*zgrid[-1]*(1.0/units.c_light - 1.0/VF_XUV),'fs')          
+            print('T0_IR =', 1e15*mn.ConvertPhoton(omega0,'omegaSI','T0SI'),'fs')           
+            print('T0_XUV =', 1e15*mn.ConvertPhoton(q*omega0,'omegaSI','T0SI'),'fs')
+
+
+            # ===============================================
+            # Complexify the fields: E(r,z,t) = Re(E_cmplx(r,z,t))
             
-            # sys.exit()
-            
-            ## Retrieve phase
+            ## Complexify the field using fft -> ifft & remove fast oscillations
             Efield_cmplx_envel = np.zeros((Nt,Nr,Nz),dtype=complex)
             rem_fast_oscillations = np.exp(-1j*omega0*tgrid)
             for k1 in range(Nz):
                 for k2 in range(Nr):
                     Efield_cmplx_envel[:,k2,k1] = rem_fast_oscillations*mn.complexify_fft(Efield[:,k2,k1])
-        
-            k_t = mn.FindInterval(tgrid, 0.0)
+
+
+            # ===============================================
+            # Retrieve the phase at the time of our interest
             
-            # phase_map = np.zeros((Nr,Nz))
-            phase_map = np.angle(Efield_cmplx_envel[k_t,:,:])
+            k_t = mn.FindInterval(tgrid, t_fix) # find time index
+            phase_map = np.angle(Efield_cmplx_envel[k_t,:,:]) # ordering (r,z)
         
             Lcoh_map = np.zeros((Nr//2,Nz-1))
+            Lcoh_map_dd = np.zeros((Nr//2,Nz-1))
             for k1 in range(Nr//2):
                 phase_r = np.unwrap(phase_map[k1,:])
                 Lcoh_map[k1, 0] = (phase_r[1] - phase_r[0]) / (zgrid[1] - zgrid[0])
                 Lcoh_map[k1, -1] = (phase_r[-1] - phase_r[-2]) / (zgrid[-1] - zgrid[-2])
-                for k2 in range(0, Nz - 1):
+                Lcoh_map_dd[k1, 0] = Lcoh_map[k1, 0]
+                Lcoh_map_dd[k1, -1] = Lcoh_map[k1, -1]
+                for k2 in range(1, Nz - 1):
                     Lcoh_map[k1, k2] = (phase_r[k2+1] - phase_r[k2]) / (zgrid[k2+1] - zgrid[k2])
+                    Lcoh_map_dd[k1, k2] = mn.ddx_arb(k2,zgrid,phase_r)
             # Lcoh_map = np.abs(np.pi / Lcoh_map)
         
                 # Lcoh_map[k1, 0] = (phase_r[1]-phase_r[0])/(zgrid[1]-zgrid[0])
@@ -155,6 +184,16 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             plt.pcolor(zgrid[:-1], rgrid[:(Nr//2)], Lcoh_map)
             plt.colorbar()
             plt.savefig('dPhidz_map_'+str(k_sim)+'.png', dpi = 600)
+            plt.show()
+            
+            plt.pcolor(zgrid[:-1], rgrid[:(Nr//2)], Lcoh_map_dd)
+            plt.colorbar()
+            plt.savefig('dPhidz_map_dd_'+str(k_sim)+'.png', dpi = 600)
+            plt.show()
+            
+            plt.pcolor(zgrid[:-1], rgrid[:(Nr//2)], Lcoh_map-Lcoh_map_dd)
+            plt.colorbar()
+            plt.savefig('dPhidz_map_error_'+str(k_sim)+'.png', dpi = 600)
             plt.show()
         
             plt.pcolor(1e3*zgrid[:-1], 1e6*rgrid[:(Nr//2)], Lcoh_map2, vmax=0.3)
