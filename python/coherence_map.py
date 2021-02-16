f8973faadd63d2cc4e3e6a91854e6e8322e33e5b
Jan Vabek <vabekjan@gmail.com>
2021-02-16 20:29:36 +0100

    added options of analyses of the coherence map.

--- python/coherence_map.py
+++ python/coherence_map.py
@@ -38,6 +38,11 @@ t_fix = 0.0 # the time of our interest to inspect e.g. phase
 fluence_source = 'computed' # options: 'file', 'computed'
 
 
+full_resolution = False
+Coherence_length = True
+Beam_analysis = False
+Efield_analysis = False
+
 rmax = 200e-6 # only for analyses
 dr = rmax/50.0
 
@@ -89,15 +94,17 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             
             print(omega0)
             tgrid = InputArchive['/outputs/tgrid'][:]; Nt = len(tgrid)
-            rgrid = InputArchive['/outputs/rgrid'][:]; Nr = len(rgrid)
-            dr_file = rgrid[1]-rgrid[0]; kr_step = max(1,int(np.floor(dr/dr_file))); Nr_max = mn.FindInterval(rgrid, rmax)
+            rgrid = InputArchive['/outputs/rgrid'][:]; Nr = len(rgrid)            
             zgrid = InputArchive['/outputs/zgrid'][:]; Nz = len(zgrid)
             
-            
-            rgrid = rgrid[0:Nr_max:kr_step]; Nr = len(rgrid)
-            electron_density_map = InputArchive['/outputs/output_plasma'][:,0:Nr_max:kr_step,:]
-            # electron_density_map_slice = InputArchive['/outputs/output_plasma'][:,0:Nr_max:kr_step,:]
+            if full_resolution:
+                kr_step = 1; Nr_max = Nr
+            else:
+                dr_file = rgrid[1]-rgrid[0]; kr_step = max(1,int(np.floor(dr/dr_file))); Nr_max = mn.FindInterval(rgrid, rmax)
+                rgrid = rgrid[0:Nr_max:kr_step]; Nr = len(rgrid)
+                
             Efield = InputArchive['/outputs/output_field'][:,0:Nr_max:kr_step,:]
+            electron_density_map = InputArchive['/outputs/output_plasma'][:,0:Nr_max:kr_step,:]            
             
             inverse_GV = InputArchive['/logs/inverse_group_velocity_SI'][()]
             VG_IR = 1.0/inverse_GV
@@ -182,67 +189,70 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             
             k_t = mn.FindInterval(tgrid, t_fix) # find time index
             phase_map = np.angle(Efield_cmplx_envel[k_t,:,:]) # ordering (r,z)
-        
-            # Compute dPhi/dz at t = t_fix
-            dPhi_dz_map = np.zeros((Nr,Nz))
-            for k1 in range(Nr):
-                phase_r = np.unwrap(phase_map[k1,:])
-                dPhi_dz_map[k1, 0] = (phase_r[1] - phase_r[0]) / (zgrid[1] - zgrid[0])
-                dPhi_dz_map[k1, -1] = (phase_r[-1] - phase_r[-2]) / (zgrid[-1] - zgrid[-2])
-                for k2 in range(1, Nz - 1):
-                    dPhi_dz_map[k1, k2] = mn.ddx_arb(k2,zgrid,phase_r)
+            
+            if Coherence_length:
+                # Compute dPhi/dz at t = t_fix
+                dPhi_dz_map = np.zeros((Nr,Nz))
+                for k1 in range(Nr):
+                    phase_r = np.unwrap(phase_map[k1,:])
+                    dPhi_dz_map[k1, 0] = (phase_r[1] - phase_r[0]) / (zgrid[1] - zgrid[0])
+                    dPhi_dz_map[k1, -1] = (phase_r[-1] - phase_r[-2]) / (zgrid[-1] - zgrid[-2])
+                    for k2 in range(1, Nz - 1):
+                        dPhi_dz_map[k1, k2] = mn.ddx_arb(k2,zgrid,phase_r)
                     
 
             # ===============================================
             # Coherence length
             
             # The coherence lenght taking only the dephasing from CUPRAD in the group-velocity frame
-            Lcoh_map2 = np.abs(np.pi/dPhi_dz_map)
+                Lcoh_map2 = np.abs(np.pi/dPhi_dz_map)
 
             # ===============================================
             # Local curvature
             
-            # Local curvature: phase evolution for a fixed z compared to the on-axis phase
-            Curvature_Gaussian_map = np.zeros((Nr,Nz)) # Gaussian beam for a comparison
-            Curvature_map = np.zeros((Nr, Nz)) # Phas from CUPRAD
-            for k1 in range(Nz):
-                if (k1 == 0):
-                    Curv_coeff = 0
-                else:
-                    Rz = zgrid[k1] + zR ** 2 / zgrid[k1]
-                    Curv_coeff = np.pi / (Rz * mn.ConvertPhoton(omega0, 'omegaSI', 'lambdaSI'))
-                Curvature_Gaussian_map[:, k1] = -Curv_coeff*(rgrid)**2
-                Curvature_map[:, k1] = np.unwrap(phase_map[:, k1])
-                Curvature_map[:, k1] = Curvature_map[:, k1] - Curvature_map[0, k1] # start always at 0
-        
-            Curvature_map = Curvature_map - np.max(Curvature_map)
+            if Beam_analysis:
+            
+                # Local curvature: phase evolution for a fixed z compared to the on-axis phase
+                Curvature_Gaussian_map = np.zeros((Nr,Nz)) # Gaussian beam for a comparison
+                Curvature_map = np.zeros((Nr, Nz)) # Phas from CUPRAD
+                for k1 in range(Nz):
+                    if (k1 == 0):
+                        Curv_coeff = 0
+                    else:
+                        Rz = zgrid[k1] + zR ** 2 / zgrid[k1]
+                        Curv_coeff = np.pi / (Rz * mn.ConvertPhoton(omega0, 'omegaSI', 'lambdaSI'))
+                    Curvature_Gaussian_map[:, k1] = -Curv_coeff*(rgrid)**2
+                    Curvature_map[:, k1] = np.unwrap(phase_map[:, k1])
+                    Curvature_map[:, k1] = Curvature_map[:, k1] - Curvature_map[0, k1] # start always at 0
+            
+                Curvature_map = Curvature_map - np.max(Curvature_map)
             
   
             # ===============================================
             # Get the fluence
             
             # Fluence is either computed from Efield or taken from the file
-            if (fluence_source == 'file'):
-                Fluence = InputArchive['/longstep/fluence'][:,:]
-                zgrid_Fluence = InputArchive['/longstep/zgrid_analyses2'][:]
-                Fluence_units = 'SI'                
-                
-            elif (fluence_source == 'computed'):                
-                zgrid_Fluence = zgrid
-                Fluence = np.zeros((Nr, Nz))
-                for k1 in range(Nz):
-                    for k2 in range(Nr):
-                        Fluence[k2, k1] = sum(abs(Efield[:, k2, k1])**2)
-                Fluence_units = 'arb.u.'
+                if (fluence_source == 'file'):
+                    Fluence = InputArchive['/longstep/fluence'][:,:]
+                    zgrid_Fluence = InputArchive['/longstep/zgrid_analyses2'][:]
+                    Fluence_units = 'SI'                
+                    
+                elif (fluence_source == 'computed'):                
+                    zgrid_Fluence = zgrid
+                    Fluence = np.zeros((Nr, Nz))
+                    for k1 in range(Nz):
+                        for k2 in range(Nr):
+                            Fluence[k2, k1] = sum(abs(Efield[:, k2, k1])**2)
+                    Fluence_units = 'arb.u.'
                 
             
             # ===============================================
             # The ionisation map at t = t_fix
             
-            ionisation_tfix_map = np.zeros((Nr, Nz))
-            for k1 in range(Nz):
-                for k2 in range(Nr):
-                    ionisation_tfix_map[k2, k1] = electron_density_map[k_t,k2,k1]
+                ionisation_tfix_map = np.zeros((Nr, Nz))
+                for k1 in range(Nz):
+                    for k2 in range(Nr):
+                        ionisation_tfix_map[k2, k1] = electron_density_map[k_t,k2,k1]
 
 
             # ===============================================
@@ -251,110 +261,115 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             # The shift is done in the Fourier space only by a phase factor,
             # we use no normalisation as it is just a mid-step.
             
-            Efield_onaxis_s = np.squeeze(Efield[:,0,:])
-            for k1 in range(Nz):
-                ogrid_nn, FE_s, NF = mn.fft_t_nonorm(tgrid, Efield_onaxis_s[:,k1]) # transform to omega space
-                
-                delta_z = zgrid[k1] # local shift
-                delta_t = inverse_GV*delta_z # shift to the laboratory frame
-                delta_t = delta_t - delta_z/units.c_light # shift to the coordinates moving by c.
-                
-                FE_s = np.exp(1j*ogrid_nn*delta_t) * FE_s # phase factor
-                
-                tnew, E_s = mn.ifft_t_nonorm(ogrid_nn,FE_s,NF)
-                Efield_onaxis_s[:,k1] = E_s.real
+            if Efield_analysis:
+                Efield_onaxis_s = np.squeeze(Efield[:,0,:])
+                for k1 in range(Nz):
+                    ogrid_nn, FE_s, NF = mn.fft_t_nonorm(tgrid, Efield_onaxis_s[:,k1]) # transform to omega space
+                    
+                    delta_z = zgrid[k1] # local shift
+                    delta_t = inverse_GV*delta_z # shift to the laboratory frame
+                    delta_t = delta_t - delta_z/units.c_light # shift to the coordinates moving by c.
+                    
+                    FE_s = np.exp(1j*ogrid_nn*delta_t) * FE_s # phase factor
+                    
+                    tnew, E_s = mn.ifft_t_nonorm(ogrid_nn,FE_s,NF)
+                    Efield_onaxis_s[:,k1] = E_s.real
                 
 
             # =================================================================
             # Print outputs
             
-            # dPhi/dz
-            plt.pcolor(zgrid, rgrid, dPhi_dz_map)
-            plt.colorbar()
-            plt.savefig('dPhidz_map_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
-            
-            # Coherence length
-            plt.pcolor(1e3*zgrid, 1e6*rgrid, Lcoh_map2, vmax=0.3)
-            plt.xlabel('z [mm]')
-            plt.ylabel('r [mum]')
-            plt.title('Lcoh [m]')
-            plt.colorbar()
-            plt.savefig('Lcoh_map_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
+            if Coherence_length:
+                
+                # dPhi/dz
+                plt.pcolor(zgrid, rgrid, dPhi_dz_map)
+                plt.colorbar()
+                plt.savefig('dPhidz_map_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
+                
+                # Coherence length
+                plt.pcolor(1e3*zgrid, 1e6*rgrid, Lcoh_map2, vmax=0.3)
+                plt.xlabel('z [mm]')
+                plt.ylabel('r [mum]')
+                plt.title('Lcoh [m]')
+                plt.colorbar()
+                plt.savefig('Lcoh_map_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
+                
+            if Coherence_length or Beam_analysis:
+                # Phase(r,z,t=t_fix) # not unwrapped, should be not difficult in a smooth case, or use some 2D-unwprapping
+                plt.pcolor(zgrid,rgrid,phase_map)
+                plt.colorbar()
+                plt.title('Phi [rad]')
+                plt.savefig('Phase_z_map_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
         
-            # Phase(r,z,t=t_fix) # not unwrapped, should be not difficult in a smooth case, or use some 2D-unwprapping
-            plt.pcolor(zgrid,rgrid,phase_map)
-            plt.colorbar()
-            plt.title('Phi [rad]')
-            plt.savefig('Phase_z_map_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
-        
-            # Curvature of the beam 
-            plt.pcolor(1e3*zgrid, 1e6*rgrid, Curvature_map, vmax = 0)
-            plt.xlabel('z [mm]')
-            plt.ylabel('r [mum]')
-            plt.title('phi_Curv [rad]')
-            plt.colorbar()
-            plt.savefig('Curvature_map_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
-        
-            # reference Gaussian curvature 
-            if Gaussian_curvature:
-                Gaussian_curvature = False
-                plt.pcolor(1e3*zgrid, 1e6*rgrid, Curvature_Gaussian_map, vmax = 0)
+            if Beam_analysis:
+                # Curvature of the beam 
+                plt.pcolor(1e3*zgrid, 1e6*rgrid, Curvature_map, vmax = 0)
                 plt.xlabel('z [mm]')
                 plt.ylabel('r [mum]')
                 plt.title('phi_Curv [rad]')
                 plt.colorbar()
-                plt.savefig('Curvature_map_Gauss.png', dpi = 600)
+                plt.savefig('Curvature_map_'+str(k_sim)+'.png', dpi = 600)
                 plt.show()
-        
-            # Fluence
-            plt.pcolor(1e3*zgrid_Fluence, 1e6*rgrid, Fluence)
-            plt.xlabel('z [mm]')
-            plt.ylabel('r [mum]')
-            plt.title('Fluence ['+Fluence_units+']')
-            plt.colorbar()
-            plt.savefig('Fluence_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
-        
-            # ionisation(r,z,t=t_fix)
-            plt.pcolor(1e3*zgrid, 1e6*rgrid, 100.0*ionisation_tfix_map/rho0_atm)
-            plt.xlabel('z [mm]')
-            plt.ylabel('r [mum]')
-            plt.title('electron density [%]')
-            plt.colorbar()
-            plt.savefig('ionisation_thalf_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
-        
-            # ionisation(r=0,z=zmax/2,t)
-            plt.plot(1e15*tgrid, 100.0*electron_density_map[:,0,Nz//2]/rho0_atm)
-            plt.xlabel('t [fs]')
-            plt.ylabel('electron density [%]')
-            plt.title('z = ' + str(1e3*zgrid[Nz//2]) + ' mm')
-            plt.savefig('ionisation_middle_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
             
+                # reference Gaussian curvature 
+                if Gaussian_curvature:
+                    Gaussian_curvature = False
+                    plt.pcolor(1e3*zgrid, 1e6*rgrid, Curvature_Gaussian_map, vmax = 0)
+                    plt.xlabel('z [mm]')
+                    plt.ylabel('r [mum]')
+                    plt.title('phi_Curv [rad]')
+                    plt.colorbar()
+                    plt.savefig('Curvature_map_Gauss.png', dpi = 600)
+                    plt.show()
             
-            # Field in the vacuum frame
-            plt.plot(1e15*tgrid, Efield_onaxis_s[:,0], linewidth=0.2, label='z=0')
-            plt.plot(1e15*tgrid, Efield_onaxis_s[:,Nz//2], linewidth=0.2, label='z=0.5zmax')
-            plt.plot(1e15*tgrid, Efield_onaxis_s[:,-1], linewidth=0.2, label='z=zmax')
-            plt.legend(loc='best')
-            plt.xlabel('t [fs]')
-            plt.ylabel('E [V/m]')
-            plt.savefig('Field_shift_lines_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
+                # Fluence
+                plt.pcolor(1e3*zgrid_Fluence, 1e6*rgrid, Fluence)
+                plt.xlabel('z [mm]')
+                plt.ylabel('r [mum]')
+                plt.title('Fluence ['+Fluence_units+']')
+                plt.colorbar()
+                plt.savefig('Fluence_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
             
-            # E(r=0,z,t) in the vacuum frame
-            plt.pcolor(1e3*zgrid, 1e15*tgrid, Efield_onaxis_s)
-            plt.xlabel('z [mm]')
-            plt.ylabel('t [fs]')
-            plt.title('shifted field [V/m]')
-            plt.colorbar()
-            plt.savefig('Field_shift_'+str(k_sim)+'.png', dpi = 600)
-            plt.show()
+                # ionisation(r,z,t=t_fix)
+                plt.pcolor(1e3*zgrid, 1e6*rgrid, 100.0*ionisation_tfix_map/rho0_atm)
+                plt.xlabel('z [mm]')
+                plt.ylabel('r [mum]')
+                plt.title('electron density [%]')
+                plt.colorbar()
+                plt.savefig('ionisation_thalf_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
+            
+                # ionisation(r=0,z=zmax/2,t)
+                plt.plot(1e15*tgrid, 100.0*electron_density_map[:,0,Nz//2]/rho0_atm)
+                plt.xlabel('t [fs]')
+                plt.ylabel('electron density [%]')
+                plt.title('z = ' + str(1e3*zgrid[Nz//2]) + ' mm')
+                plt.savefig('ionisation_middle_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
+            
+            if Efield_analysis:
+                # Field in the vacuum frame
+                plt.plot(1e15*tgrid, Efield_onaxis_s[:,0], linewidth=0.2, label='z=0')
+                plt.plot(1e15*tgrid, Efield_onaxis_s[:,Nz//2], linewidth=0.2, label='z=0.5zmax')
+                plt.plot(1e15*tgrid, Efield_onaxis_s[:,-1], linewidth=0.2, label='z=zmax')
+                plt.legend(loc='best')
+                plt.xlabel('t [fs]')
+                plt.ylabel('E [V/m]')
+                plt.savefig('Field_shift_lines_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
+                
+                # E(r=0,z,t) in the vacuum frame
+                plt.pcolor(1e3*zgrid, 1e15*tgrid, Efield_onaxis_s)
+                plt.xlabel('z [mm]')
+                plt.ylabel('t [fs]')
+                plt.title('shifted field [V/m]')
+                plt.colorbar()
+                plt.savefig('Field_shift_'+str(k_sim)+'.png', dpi = 600)
+                plt.show()
 
 
     df_delta_t_columns_tup = df_delta_t_columns.tolist()
