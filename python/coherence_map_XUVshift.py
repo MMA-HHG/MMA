Fixed over-allocation and cutoff calculation added
We arrived to the possibility that CUPRAD over-allocates the array for outputs at the beginning as we made this as a sefety factor. Python then should read only slice corresponding to zgrig, that is allways of the right size. The second edit just adds the HHG cut-off as an output analysis.

python/coherence_map_XUVshift.py
@@ -8,6 +8,7 @@ import sys
 sys.path.append('D:\git\python_modules')
 import units
 import mynumerics as mn
+import HHG
 # import mynumerics as mn
 import matplotlib.pyplot as plt
 import re
@@ -118,8 +119,8 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
                 dr_file = rgrid[1]-rgrid[0]; kr_step = max(1,int(np.floor(dr/dr_file))); Nr_max = mn.FindInterval(rgrid, rmax)
                 rgrid = rgrid[0:Nr_max:kr_step]; Nr = len(rgrid)
                 
-            Efield = InputArchive['/outputs/output_field'][:,0:Nr_max:kr_step,:]
-            electron_density_map = InputArchive['/outputs/output_plasma'][:,0:Nr_max:kr_step,:]            
+            Efield = InputArchive['/outputs/output_field'][:,0:Nr_max:kr_step,:Nz] # Arrays may be over-allocated by CUPRAD
+            electron_density_map = InputArchive['/outputs/output_plasma'][:,0:Nr_max:kr_step,:Nz]            
             
             inverse_GV = InputArchive['/logs/inverse_group_velocity_SI'][()]
             VG_IR = 1.0/inverse_GV
@@ -129,6 +130,9 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
         
             rho0_init = 1e6 * mn.readscalardataset(InputArchive, '/inputs/calculated/medium_effective_density_of_neutral_molecules','N')
             pressure = InputArchive['/inputs/medium_pressure_in_bar'][()]
+            
+            Intensity_entry = InputArchive['/inputs/laser_intensity_entry'][()]
+            Ip_eV = InputArchive['/inputs/ionization_ionization_potential_of_neutral_molecules'][()]
             
             pre_ion_ratio = InputArchive['/pre_ionised/initial_electrons_ratio'][()]
             I0 = InputArchive['/inputs/laser_intensity_entry'][()]
@@ -178,8 +182,21 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             dset_id = grp.create_dataset('T0_IR', data=1e15*mn.ConvertPhoton(omega0,'omegaSI','T0SI'))
             dset_id.attrs['units'] = np.string_('[fs]')            
 
-            dset_id = grp.create_dataset('T0_XUV', data=1e15*mn.ConvertPhoton(q*omega0,'omegaSI','T0SI'))
+            dset_id = grp.create_dataset('investigated_harmonic_orders', data=np.asarray(Horders))
+            dset_id.attrs['units'] = np.string_('[-]')
+            
+            T0_XUV = []
+            for k1 in range(NH): T0_XUV.append(1e15*mn.ConvertPhoton(q*omega0,'omegaSI','T0SI'))
+            dset_id = grp.create_dataset('T0_XUV', data=np.asarray(T0_XUV))
             dset_id.attrs['units'] = np.string_('[fs]')
+                
+            dset_id = grp.create_dataset('entry_cutoff',
+                                         data=HHG.ComputeCutoff(
+                                              Intensity_entry/units.INTENSITYau,
+                                              mn.ConvertPhoton(omega0,'omegaSI','omegaau'),
+                                              mn.ConvertPhoton(Ip_eV,'eV','omegaau')
+                                              )[1])
+            dset_id.attrs['units'] = np.string_('[-]')               
 
             print('characteristic velocities')
             print(units.c_light,VF_IR,1.0/inverse_GV,VF_XUV, sep='\n')

python/compare_n_results_XUVshift.py
@@ -43,7 +43,7 @@ files = ['results_1.h5', 'results_4.h5', 'results_7.h5']
 
 files = ['results_1.h5', 'results_10.h5']
 files = ['results_1.h5', 'results_2.h5', 'results_3.h5']
-
+files = ['results_12.h5', 'results_13.h5']
 
 # labels = ['p=15 mbar', 'p=35 mbar'], ['Pi=0 %', 'Pi=4 %','Pi=8 %', 'Pi=12 %', 'Pi=16 %'], ['I0=1e14 W/cm2', 'I0=1.75e14 W/cm2', 'I0=2.5e14 W/cm2']
 labels = ['a','b','c','d','e','f']
@@ -115,21 +115,25 @@ with h5py.File(out_h5name,'w') as OutFile:
         
         for k1 in range(Nfiles):
             # dum = InArch[k1]['/outputs/output_field'][:,0,:]
-            Efield_onaxis_s.append(InArch[k1]['/outputs/output_field'][:,0,:])
-            Efield_onaxis_s_XUV.append(np.zeros(Efield_onaxis_s[k1].shape))
             
             # Efield_s.append(InArch[k1]['/outputs/output_field'][:])
             # Efield_s_XUV.append(np.zeros(Efield_s[k1].shape))
             
             
-            zgrid.append(InArch[k1]['/outputs/zgrid'][:])
+            zgrid.append(InArch[k1]['/outputs/zgrid'][:]); Nz_loc=len(zgrid[k1])
+            
+            # We arrived to the problem with possible over-allocation of Efield in CUPRAD
+            Efield_onaxis_s.append(InArch[k1]['/outputs/output_field'][:,0,:Nz_loc])
+            Efield_onaxis_s_XUV.append(np.zeros(Efield_onaxis_s[k1].shape))
+            
+            
             tgrid.append(InArch[k1]['/outputs/tgrid'][:])
             omega0 = mn.ConvertPhoton(1e-2*mn.readscalardataset(InArch[0],'/inputs/laser_wavelength','N'),'lambdaSI','omegaSI')
             inverse_GV.append(InArch[k1]['/logs/inverse_group_velocity_SI'][()])
             pressure.append(InArch[k1]['/inputs/medium_pressure_in_bar'][()])
             
             Intensity_entry.append(InArch[k1]['/inputs/laser_intensity_entry'][()])
-            Ip_eV.append(InArch[k1]['/inputs/ionization_ionization_poential_of_neutral_molecules'][()])
+            Ip_eV.append(InArch[k1]['/inputs/ionization_ionization_potential_of_neutral_molecules'][()])
             
             rho0_init = 1e6 * mn.readscalardataset(InArch[k1], '/inputs/calculated/medium_effective_density_of_neutral_molecules','N')
             