393d3677d7a21ee491e0518d56a6633374b6790d
Jan Vabek <vabekjan@gmail.com>
2021-03-10 21:08:41 +0100

    created sbatch submitting script using flags

--- testing/job_scheduler/inps/post_process.sh
+++ testing/job_scheduler/inps/post_process.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-sbatch $TESTPATH/plot1.slurm
+while getopts :t: flags; do
+  case $flags in
+    t)
+      analysis=${OPTARG}
+      echo "-a was triggered, Parameter: $OPTARG"
+      ;;
+    # \?)
+    #   echo "Invalid option: -$OPTARG"
+    #   exit 1
+    #   ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+    #   exit 1
+      ;;
+  esac
+done
+
+# echo $analysis
+
+case $analysis in
+    general)
+      sbatch $TESTPATH/coherence_map_XUVshift.slurm
+    #   echo 'plot 1'
+      ;;
+    compare)
+      sbatch $TESTPATH/compare_n_results_XUVshift.slurm
+    #   echo 'plot 2'
+      ;;
+esac
+
+# sbatch $TESTPATH/plot1.slurm
 
 echo "done" 
