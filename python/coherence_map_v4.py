858752e2c556279a5c9fc0827688ccf469ea573f
Jan Vabek <vabekjan@gmail.com>
2021-04-12 15:20:22 +0200

    Data as a class.

--- python/coherence_map_v4.py
+++ python/coherence_map_v4.py
@@ -166,70 +166,52 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
         print('processing:', file_path)             
         with h5py.File(file_path, 'r') as InputArchive:
             
-            test_import = dfC.get_data(InputArchive, r_resolution = [full_resolution, dr, rmax])
-            
-            omega0 = mn.ConvertPhoton(1e-2*mn.readscalardataset(InputArchive,'/inputs/laser_wavelength','N'),'lambdaSI','omegaSI')
-            k0_wave = 2.0*np.pi/mn.ConvertPhoton(omega0,'omegaSI','lambdaSI')
-            tgrid = InputArchive['/outputs/tgrid'][:]; Nt = len(tgrid)
-            rgrid = InputArchive['/outputs/rgrid'][:]; Nr = len(rgrid)            
-            zgrid = InputArchive['/outputs/zgrid'][:]; Nz = len(zgrid)            
-            if full_resolution:
-                kr_step = 1; Nr_max = Nr
-            else:
-                dr_file = rgrid[1]-rgrid[0]; kr_step = max(1,int(np.floor(dr/dr_file))); Nr_max = mn.FindInterval(rgrid, rmax)
-                rgrid = rgrid[0:Nr_max:kr_step]; Nr = len(rgrid)                
-            E_trz = InputArchive['/outputs/output_field'][:,0:Nr_max:kr_step,:Nz] # Arrays may be over-allocated by CUPRAD
-            inverse_GV = InputArchive['/logs/inverse_group_velocity_SI'][()]
-            VG_IR = 1.0/inverse_GV               
-            rho0_init = 1e6 * mn.readscalardataset(InputArchive, '/inputs/calculated/medium_effective_density_of_neutral_molecules','N')
-            Ip_eV = InputArchive['/inputs/ionization_ionization_potential_of_neutral_molecules'][()]
-            pressure_mbar = 1e3*InputArchive['/inputs/medium_pressure_in_bar'][()]; pressure_string = "{:.1f}".format(pressure_mbar)+' mbar'
-            preionisation_ratio = InputArchive['/pre_ionised/initial_electrons_ratio'][()]; preionisation_string = "{:.1f}".format(100*preionisation_ratio) + '%'
+            res = dfC.get_data(InputArchive, r_resolution = [full_resolution, dr, rmax])
             
             ### Shift to the vacuum frame
             if vacuum_frame:
-                E_vac = np.zeros(E_trz.shape)   
-                for k1 in range(Nz):
-                    delta_z = zgrid[k1] # local shift
-                    delta_t_lab = inverse_GV*delta_z # shift to the laboratory frame
+                E_vac = np.zeros(res.E_trz.shape)   
+                for k1 in range(res.Nz):
+                    delta_z = res.zgrid[k1] # local shift
+                    delta_t_lab = res.inverse_GV*delta_z # shift to the laboratory frame
                     delta_t_vac = delta_t_lab - delta_z/units.c_light # shift to the coordinates moving by c.
-                    for k2 in range(Nr):
-                        ogrid_nn, FE_s, NF = mn.fft_t_nonorm(tgrid, E_trz[:,k2,k1]) # transform to omega space        
+                    for k2 in range(res.Nr):
+                        ogrid_nn, FE_s, NF = mn.fft_t_nonorm(res.tgrid, res.E_trz[:,k2,k1]) # transform to omega space        
                         FE_s = np.exp(1j*ogrid_nn*delta_t_vac) * FE_s # phase factor        
                         tnew, E_s = mn.ifft_t_nonorm(ogrid_nn,FE_s,NF)
                         E_vac[:,k2,k1] = E_s.real
                     
-                E_trz = E_vac          
+                res.E_trz = E_vac          
 
             # ===============================================
             # Complexify the fields: E(r,z,t) = Re(E_cmplx(r,z,t))
-            E_trz_cmplx_envel = np.zeros(E_trz.shape,dtype=complex)
-            rem_fast_oscillations = np.exp(-1j*omega0*tgrid)
+            E_trz_cmplx_envel = np.zeros(res.E_trz.shape,dtype=complex)
+            rem_fast_oscillations = np.exp(-1j*res.omega0*res.tgrid)
             
-            for k1 in range(Nz):
-                for k2 in range(Nr):
-                    E_trz_cmplx_envel[:,k2,k1] = rem_fast_oscillations*mn.complexify_fft(E_trz[:,k2,k1])
+            for k1 in range(res.Nz):
+                for k2 in range(res.Nr):
+                    E_trz_cmplx_envel[:,k2,k1] = rem_fast_oscillations*mn.complexify_fft(res.E_trz[:,k2,k1])
 
 
             # ===============================================
             # Retrieve the phase at the time of our interest
-            t_probe_ind = mn.FindInterval(tgrid, t_probe) # find time indices
+            t_probe_ind = mn.FindInterval(res.tgrid, t_probe) # find time indices
             
             phase = np.angle(E_trz_cmplx_envel[t_probe_ind,:,:]) # ordering (r,z)
             Intens = mn.FieldToIntensitySI(abs(E_trz_cmplx_envel))
-            grad_z_I = np.gradient(Intens[t_probe_ind,:,:],zgrid,axis=2,edge_order=2)
+            grad_z_I = np.gradient(Intens[t_probe_ind,:,:],res.zgrid,axis=2,edge_order=2)
             
             grad_z_phase = np.zeros(phase.shape)
-            for k1 in range(Nr):
+            for k1 in range(res.Nr):
                 for k2 in range(Nt_probe):
                     phase_rfix = np.unwrap(phase[k2,k1,:])
-                    grad_z_phase[k2,k1,:] = np.gradient(phase_rfix,zgrid,edge_order=2)
+                    grad_z_phase[k2,k1,:] = np.gradient(phase_rfix,res.zgrid,edge_order=2)
                     
             nXUV = [];
             for k1 in range(NH):
                 q = Horders[k1]
-                f1 = XUV_index.getf1(gas_type+'_'+XUV_table_type, mn.ConvertPhoton(q*omega0, 'omegaSI', 'eV'))
-                nXUV.append(1.0 - rho0_init*units.r_electron_classical*(mn.ConvertPhoton(q*omega0,'omegaSI','lambdaSI')**2)*f1/(2.0*np.pi))
+                f1 = XUV_index.getf1(gas_type+'_'+XUV_table_type, mn.ConvertPhoton(q*res.omega0, 'omegaSI', 'eV'))
+                nXUV.append(1.0 - res.rho0_init*units.r_electron_classical*(mn.ConvertPhoton(q*res.omega0,'omegaSI','lambdaSI')**2)*f1/(2.0*np.pi))
             
             FSPA_alphas = []; grad_z_phase_FSPA = []
             for k1 in range(NH):
@@ -239,23 +221,23 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
                 
                           # Cut-off map
             Cutoff = HHG.ComputeCutoff(Intens/units.INTENSITYau,
-                                       mn.ConvertPhoton(omega0,'omegaSI','omegaau'),
-                                       mn.ConvertPhoton(Ip_eV,'eV','omegaau')
+                                       mn.ConvertPhoton(res.omega0,'omegaSI','omegaau'),
+                                       mn.ConvertPhoton(res.Ip_eV,'eV','omegaau')
                                        )[1]
               
             # loop outputs over times and harmonic orders
             fig1, ax1 = plt.subplots()
             fig2, ax2 = plt.subplots()
             fig3, ax3 = plt.subplots()
-            title_string = ', ' + preionisation_string + ', ' + pressure_string
+            title_string = ', ' + res.preionisation_string + ', ' + res.pressure_string
             for k1 in range(Nt_probe):
-              t_string = ', '+"{:.1f}".format(1e15*tgrid[t_probe_ind[k1]])+' fs'
+              t_string = ', '+"{:.1f}".format(1e15*res.tgrid[t_probe_ind[k1]])+' fs'
             
               Cutoff_loc = Cutoff[t_probe_ind[k1],:,:]
 
               fig7, ax7 = plt.subplots()
-              map1 = ax7.pcolor(1e3*zgrid, 1e6*rgrid, Cutoff[t_probe_ind[k1],:,:], shading='auto')
-              map2 = ax7.contour(1e3*zgrid, 1e6*rgrid, Cutoff[t_probe_ind[k1],:,:], Horders, colors = "black")
+              map1 = ax7.pcolor(1e3*res.zgrid, 1e6*res.rgrid, Cutoff[t_probe_ind[k1],:,:], shading='auto')
+              map2 = ax7.contour(1e3*res.zgrid, 1e6*res.rgrid, Cutoff[t_probe_ind[k1],:,:], Horders, colors = "black")
               ax7.set_xlabel('z [mm]'); ax7.set_ylabel('r [mum]'); ax7.set_title('Cutoff'+title_string+t_string )
               fig7.colorbar(map1) 
               fig7.savefig('Cutoff_t'+str(k1)+'_sim'+str(k_sim)+'.png', dpi = 600)
@@ -272,26 +254,26 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
                 
                 # onax
                 if (linestyles_plt[k1] == '-'):              
-                  ax1.plot(1e3*zgrid,q*(grad_z_phase[k1,0,:] + k0_wave*(nXUV[k2]-1)),label='H'+str(q),
+                  ax1.plot(1e3*res.zgrid,q*(grad_z_phase[k1,0,:] + res.k0_wave*(nXUV[k2]-1)),label='H'+str(q),
                          color=colors_plt[k2], linestyle=linestyles_plt[k1])               
-                  ax2.plot(1e3*zgrid,grad_z_phase_FSPA[k2][k1,0,:],label='H'+str(q),
+                  ax2.plot(1e3*res.zgrid,grad_z_phase_FSPA[k2][k1,0,:],label='H'+str(q),
                          color=colors_plt[k2], linestyle=linestyles_plt[k1])                
-                  ax3.plot(1e3*zgrid,
-                         q*(grad_z_phase[k1,0,:] + k0_wave*(nXUV[k2]-1)) + grad_z_phase_FSPA[k2][k1,0,:],
+                  ax3.plot(1e3*res.zgrid,
+                         q*(grad_z_phase[k1,0,:] + res.k0_wave*(nXUV[k2]-1)) + grad_z_phase_FSPA[k2][k1,0,:],
                          label='H'+str(q), color=colors_plt[k2], linestyle=linestyles_plt[k1])
                 else:
-                  ax1.plot(1e3*zgrid,q*(grad_z_phase[k1,0,:] + k0_wave*(nXUV[k2]-1)),
+                  ax1.plot(1e3*res.zgrid,q*(grad_z_phase[k1,0,:] + res.k0_wave*(nXUV[k2]-1)),
                          color=colors_plt[k2], linestyle=linestyles_plt[k1])               
-                  ax2.plot(1e3*zgrid,grad_z_phase_FSPA[k2][k1,0,:],
+                  ax2.plot(1e3*res.zgrid,grad_z_phase_FSPA[k2][k1,0,:],
                          color=colors_plt[k2], linestyle=linestyles_plt[k1])                
-                  ax3.plot(1e3*zgrid,
-                         q*(grad_z_phase[k1,0,:] + k0_wave*(nXUV[k2]-1)) + grad_z_phase_FSPA[k2][k1,0,:],
+                  ax3.plot(1e3*res.zgrid,
+                         q*(grad_z_phase[k1,0,:] + res.k0_wave*(nXUV[k2]-1)) + grad_z_phase_FSPA[k2][k1,0,:],
                          color=colors_plt[k2], linestyle=linestyles_plt[k1])
                   
                   
                 fig4, ax4 = plt.subplots()                
-                masked = np.ma.masked_where(np.isnan(H_mask), q*(grad_z_phase[k1,:,:] + k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:] )                
-                map1 = ax4.pcolor(1e3*zgrid, 1e6*rgrid,
+                masked = np.ma.masked_where(np.isnan(H_mask), q*(grad_z_phase[k1,:,:] + res.k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:] )                
+                map1 = ax4.pcolor(1e3*res.zgrid, 1e6*res.rgrid,
                                   masked, # q*(grad_z_phase[k1,:,:] + k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:],
                                      shading='auto')
                 ax4.set_xlabel('z [mm]'); ax4.set_ylabel('r [mum]'); ax4.set_title('dPhi/dz, full, H'+str(q)+title_string+t_string ) 
@@ -300,8 +282,8 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
                 
                 fig8, ax8 = plt.subplots()
                 
-                masked = np.ma.masked_where(np.isnan(H_mask), abs(q*(grad_z_phase[k1,:,:] + k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:]))
-                map1 = ax8.pcolor(1e3*zgrid, 1e6*rgrid,
+                masked = np.ma.masked_where(np.isnan(H_mask), abs(q*(grad_z_phase[k1,:,:] + res.k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:]))
+                map1 = ax8.pcolor(1e3*res.zgrid, 1e6*res.rgrid,
                                   masked,
                                      shading='auto',vmin=0.0)
                 ax8.set_xlabel('z [mm]'); ax8.set_ylabel('r [mum]'); ax8.set_title('|dPhi/dz|, full, H'+str(q)+title_string+t_string ) 
@@ -309,12 +291,12 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
                 fig8.savefig('abs_dPhi_dz_t'+str(k1)+'_H'+str(q)+'_sim'+str(k_sim)+'.png', dpi = 600)
                 
                 fig5, ax5 = plt.subplots()
-                dum = abs( 1.0/ (q*(grad_z_phase[k1,:,:] + k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:] ) )
-                masked = np.ma.masked_where(np.isnan(H_mask), abs( 1.0/ (q*(grad_z_phase[k1,:,:] + k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:] ) ))
+                dum = abs( 1.0/ (q*(grad_z_phase[k1,:,:] + res.k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:] ) )
+                masked = np.ma.masked_where(np.isnan(H_mask), abs( 1.0/ (q*(grad_z_phase[k1,:,:] + res.k0_wave*(nXUV[k2]-1))+grad_z_phase_FSPA[k2][k1,:,:] ) ))
                 if (np.max(dum) > Lcoh_saturation):
-                    map1 = ax5.pcolor(1e3*zgrid, 1e6*rgrid, masked, shading='auto', vmax=Lcoh_saturation)
+                    map1 = ax5.pcolor(1e3*res.zgrid, 1e6*res.rgrid, masked, shading='auto', vmax=Lcoh_saturation)
                 else:
-                    map1 = ax5.pcolor(1e3*zgrid, 1e6*rgrid, masked, shading='auto')
+                    map1 = ax5.pcolor(1e3*res.zgrid, 1e6*res.rgrid, masked, shading='auto')
                 
                 ax5.set_xlabel('z [mm]'); ax5.set_ylabel('r [mum]'); ax5.set_title('Lcoh, H'+str(q)+title_string+t_string )         
                 fig5.colorbar(map1)
@@ -335,19 +317,19 @@ with h5py.File(out_h5name,'w') as OutFile: # this file contains numerical analys
             
             # Intnesity shape
             fig9, ax9 = plt.subplots()
-            ax9.plot(1e15*tgrid,Cutoff[:,0,0], color=colors_plt[0],label='z='+"{:.1f}".format(1e3*zgrid[0])) 
-            ax9.plot(1e15*tgrid,Cutoff[:,0,(Nz-1)//2], color=colors_plt[1],label='z='+"{:.1f}".format(1e3*zgrid[(Nz-1)//2])) 
-            ax9.plot(1e15*tgrid,Cutoff[:,0,-1], color=colors_plt[2],label='z='+"{:.1f}".format(1e3*zgrid[-1]))
+            ax9.plot(1e15*res.tgrid,Cutoff[:,0,0], color=colors_plt[0],label='z='+"{:.1f}".format(1e3*res.zgrid[0])) 
+            ax9.plot(1e15*res.tgrid,Cutoff[:,0,(res.Nz-1)//2], color=colors_plt[1],label='z='+"{:.1f}".format(1e3*res.zgrid[(res.Nz-1)//2])) 
+            ax9.plot(1e15*res.tgrid,Cutoff[:,0,-1], color=colors_plt[2],label='z='+"{:.1f}".format(1e3*res.zgrid[-1]))
             ax9.set_xlim(tlim)
             ax9.legend(loc='best')
             ax9.set_xlabel('t [fs]'); ax9.set_ylabel('I [cutoff]'); ax9.set_title('onaxis intensity'+title_string)   
             fig9.savefig('Intens_onax_sim'+str(k_sim)+'.png', dpi = 600)
             
-            k_t = mn.FindInterval(tgrid, 0.0)
+            k_t = mn.FindInterval(res.tgrid, 0.0)
             fig10, ax10 = plt.subplots()
-            ax10.plot(1e6*rgrid,Cutoff[k_t,:,0], color=colors_plt[0],label='z='+"{:.1f}".format(1e3*zgrid[0])) 
-            ax10.plot(1e6*rgrid,Cutoff[k_t,:,(Nz-1)//2], color=colors_plt[1],label='z='+"{:.1f}".format(1e3*zgrid[(Nz-1)//2])) 
-            ax10.plot(1e6*rgrid,Cutoff[k_t,:,-1], color=colors_plt[2],label='z='+"{:.1f}".format(1e3*zgrid[-1]))
+            ax10.plot(1e6*res.rgrid,Cutoff[k_t,:,0], color=colors_plt[0],label='z='+"{:.1f}".format(1e3*res.zgrid[0])) 
+            ax10.plot(1e6*res.rgrid,Cutoff[k_t,:,(res.Nz-1)//2], color=colors_plt[1],label='z='+"{:.1f}".format(1e3*res.zgrid[(res.Nz-1)//2])) 
+            ax10.plot(1e6*res.rgrid,Cutoff[k_t,:,-1], color=colors_plt[2],label='z='+"{:.1f}".format(1e3*res.zgrid[-1]))
             ax10.legend(loc='best')
             ax10.set_xlabel('r [mum]'); ax10.set_ylabel('I [cutoff]'); ax10.set_title('t=0 fs, intensity'+title_string)   
             fig10.savefig('Intens_tfix_sim'+str(k_sim)+'.png', dpi = 600)
