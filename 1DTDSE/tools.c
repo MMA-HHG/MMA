92aa838725019d8b5b77dbc2f149e4ba16a608e5
Jan Vabek <vabekjan@gmail.com>
2021-05-17 14:24:54 +0200

    Attempt to construct the printing driver.

--- 1DTDSE/tools.c
+++ 1DTDSE/tools.c
@@ -4,8 +4,10 @@
 #include<stdio.h>
 #include<time.h>
 
+#include "hdf5.h"
 #include "numerical_constants.h"
 #include "util.h"
+#include "util_hdf5.h"
 
 clock_t start, finish;
 clock_t start2, finish2;
@@ -68,6 +70,62 @@ struct output_print_def Set_all_prints(void)
 	res.omegagrid = 1;
 	res.PopInt = 1;
 	res.expval_x = 1;
+
+	return res;
+}
+
+struct output_print_def Set_prints_from_HDF5(hid_t file_id)
+{
+	struct output_print_def res;
+	char path[50];
+	int dum_int;
+
+	res = output_print_def Initialise_Printing_struct();
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_Efield");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.Efield = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_Source_Term");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.sourceterm = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_F_Source_Term");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.Fsourceterm = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_F_Efield_M2");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.FEfieldM2 = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_F_Source_Term_M2");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.FsourceTermM2 = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_GS_population");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.PopTot = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_integrated_population");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.PopInt = 1;}
+
+	path[0] = '\0';	strcat(strcat(path,inpath),"print_x_expectation_value");
+	readint(file_id, path, h5error,&dum_int);
+	if(dum_int==1){res.expval_x = 1;}
+	
+	// res.FEfield = 1;
+	// res.sourceterm = 1;
+	// res.Fsourceterm = 1;
+	// res.FEfieldM2 = 1;
+	// res.FsourceTermM2 = 1;
+	// res.PopTot = 1;
+
+	res.tgrid = 1; // not memory consuming
+	res.omegagrid = 1; // not memory consuming
+
+	// res.PopInt = 1;
+	// res.expval_x = 1;
 
 	return res;
 }
