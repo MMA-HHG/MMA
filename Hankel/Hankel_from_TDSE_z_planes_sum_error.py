e0a5bf3c57a3dc47d848fe30e83aac46cd782955
Jan Vabek <vabekjan@gmail.com>
2021-06-18 00:32:13 +0200

    attempt to compute Hankels by the exact trapezoidal rule

--- Hankel/Hankel_from_TDSE_z_planes_sum_error.py
+++ Hankel/Hankel_from_TDSE_z_planes_sum_error.py
@@ -26,13 +26,13 @@ else:
     results_CUPRAD = os.path.join("D:\data", "Discharges", "TDSE", "t6")
     results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSEH1")
     results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSE40planes1")
-    results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSE50planes1")
+    results_TDSE = os.path.join("C:\data", "Discharges", "TDSE", "TDSE50planes1") # fine up to 46
     
     # results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSE10planes1")
 
 
 file_CUPRAD = 'results_1.h5'
-file_TDSE = 'results_merged.h5' # 'hdf5_temp_0000000.h5'
+file_TDSE = 'results_merged_Fsource.h5' # 'hdf5_temp_0000000.h5'
 
 
 file_CUPRAD = os.path.join(results_CUPRAD,file_CUPRAD)
@@ -57,42 +57,45 @@ with h5py.File(file_CUPRAD, 'r') as InputArchiveCUPRAD, h5py.File(file_TDSE, 'r'
    
    # GS_init = InputArchiveTDSE['ground_state'][:,0] + 1j*InputArchiveTDSE['ground_state'][:,1]
 
+print('data loaded:')
 
 Nr_max = 235 #470; 235; 155-still fine
 kr_step = 2 # descending order, tha last is "the most accurate"
-ko_step = 2
+ko_step = 5
 
 rmax_FF = 8*1e-4
-Nr_FF = 800
+Nr_FF = 200
 
 FF_orders_plot = 10
 
 omega_au2SI = mn.ConvertPhoton(1.0, 'omegaau', 'omegaSI')
 ogridSI = omega_au2SI * ogrid
 
-Hgrid = ogrid/omega0
-Hvalue = 17 # [14, 36]
 
-Nz_max_sum = 33
+Hgrid = ogrid/omega0
+Hrange = [17, 18] # [14, 36]
+H_indices = [mn.FindInterval(Hgrid,Hvalue) for Hvalue in Hrange]
+
+Nz_max_sum = 15
 kz_steps = [8,4,2,1] # descending order, tha last is "the most accurate"
 
-H_index = mn.FindInterval(Hgrid,Hvalue)
+# H_index = mn.FindInterval(Hgrid,Hvalue)
 
 rgrid_FF = np.linspace(0.0, rmax_FF, Nr_FF)
-ogrid_select_SI = [ogridSI[H_index]]
+ogrid_select_SI = ogridSI[H_indices[0]:H_indices[1]:ko_step]
 
 
 FField_FF = []
-for k1 in range(len(zgrid_macro)):
+for k1 in range(Nz_max_sum):
     
     # FSourceTerm_select = np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_index]).T
     
-    if (k1 == 0):  # to enforce correct dimensions
-         dum = np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_index]).T
-         FSourceTerm_select = np.zeros((1,)+dum.shape, dtype=np.cdouble)
-         FSourceTerm_select[0,:] = dum # np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_index]).T
-    else:
-         FSourceTerm_select[0,:] = np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_index]).T
+    # if (k1 == 0):  # to enforce correct dimensions
+    #      dum = np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_indices[0]:H_indices[1]:ko_step]).T
+    #      FSourceTerm_select = np.zeros((1,)+dum.shape, dtype=np.cdouble)
+    #      FSourceTerm_select = dum # np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_index]).T
+    # else:
+    FSourceTerm_select = np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_indices[0]:H_indices[1]:ko_step]).T
          
     # FSourceTerm_select[np.newaxis] # to enforce correct dimensions
     # FSourceTerm_select.reshape((1,) + FSourceTerm_select.shape) # to enforce correct dimensions
@@ -105,60 +108,120 @@ for k1 in range(len(zgrid_macro)):
                                      0.3,
                                      rgrid_FF)
     
-    if (k1 == 0): FField_FF_z = np.zeros( (len(zgrid_macro),) + FField_FF.shape,dtype=np.cdouble)  
+    if (k1 == 0): FField_FF_z = np.zeros( (Nz_max_sum,) + FField_FF.shape,dtype=np.cdouble)  
     FField_FF_z[k1,:,:] = FField_FF                    
 
 
-FField_FF_z_sum = []
+
+# FField_FF_z_sum = []
+# for k1 in range(len(kz_steps)):
+#     FField_FF_z_sum.append(FField_FF_z[0,:,:])
+#     for k2 in range(kz_steps[k1],Nz_max_sum,kz_steps[k1]):
+#         FField_FF_z_sum[k1] = FField_FF_z_sum[k1] + FField_FF_z[k2,:,:]
+#     FField_FF_z_sum[k1] = FField_FF_z_sum[k1] / len(range(0,Nz_max_sum,kz_steps[k1]))
+    
+FField_FF_z_sum = [] # trapezoid
 for k1 in range(len(kz_steps)):
-    FField_FF_z_sum.append(FField_FF_z[0,0,:])
+    # FField_FF_z_sum.append(FField_FF_z[0,:,:])
+    dum = 0.5*(zgrid_macro[kz_steps[k1]]-zgrid_macro[0]) * (FField_FF_z[0,:,:] + FField_FF_z[kz_steps[k1],:,:])
     for k2 in range(kz_steps[k1],Nz_max_sum,kz_steps[k1]):
-        FField_FF_z_sum[k1] = FField_FF_z_sum[k1] + FField_FF_z[k2,0,:]
-    FField_FF_z_sum[k1] = FField_FF_z_sum[k1] / len(range(0,Nz_max_sum,kz_steps[k1]))
+        dum = dum + \
+              0.5*(zgrid_macro[k2]-zgrid_macro[0]) * (FField_FF_z[0,:,:] + FField_FF_z[kz_steps[k1],:,:])
+    dum = dum / len(range(0,Nz_max_sum,kz_steps[k1]))
+    FField_FF_z_sum.append(dum)
 
 
-fig = plt.figure()
-plt.plot(rgrid_FF,abs(FField_FF_z_sum[0]))
-plt.plot(rgrid_FF,abs(FField_FF_z_sum[1]))
-plt.plot(rgrid_FF,abs(FField_FF_z_sum[2]))
-plt.plot(rgrid_FF,abs(FField_FF_z_sum[3]))
+Hankel_errors = []
+for k1 in range(len(kz_steps)-1):
+    Hankel_errors.append(
+                         (FField_FF_z_sum[k1+1]-FField_FF_z_sum[k1])/np.max(abs(FField_FF_z_sum[k1+1]))
+                         )
+    
+Hgrid_select = Hgrid[H_indices[0]:H_indices[1]:ko_step]
+
+# vmin = np.max(np.log(Gaborr))-6.
+fig, ax = plt.subplots()  
+Hankel_errors_logscale = np.log(abs(Hankel_errors[0].T))
+map1 = ax.pcolor(Hgrid_select,rgrid_FF,Hankel_errors_logscale, shading='auto')
+# plt.pcolor(t_Gr,o_Gr/omega0,(np.log(Gaborr)).T, shading='auto',vmin=vmin)
+fig.colorbar(map1)
+# dr_string = ', '+"{:.1f}".format(1e6*rgrid_macro[kr_steps[0]]) +' mum'
+# plt.title('Error' + dr_string)
+plt.xlabel('H [-]')
+plt.ylabel('r [m]')
 plt.show()
+# plt.close(fig)
 
-fig = plt.figure()
-dum = np.max(abs(FField_FF_z_sum[-1]))
-plt.semilogy(rgrid_FF,abs(FField_FF_z_sum[0]-FField_FF_z_sum[-1])/dum)
-plt.semilogy(rgrid_FF,abs(FField_FF_z_sum[1]-FField_FF_z_sum[-1])/dum)
-plt.semilogy(rgrid_FF,abs(FField_FF_z_sum[2]-FField_FF_z_sum[-1])/dum)
+# vmin = np.max(np.log(Gaborr))-6.
+fig, ax = plt.subplots()  
+Hankel_errors_logscale = np.log(abs(Hankel_errors[1].T))
+map1 = ax.pcolor(Hgrid_select,rgrid_FF,Hankel_errors_logscale, shading='auto')
+# plt.pcolor(t_Gr,o_Gr/omega0,(np.log(Gaborr)).T, shading='auto',vmin=vmin)
+fig.colorbar(map1)
+# dr_string = ', '+"{:.1f}".format(1e6*rgrid_macro[kr_steps[0]]) +' mum'
+# plt.title('Error' + dr_string)
+plt.xlabel('H [-]')
+plt.ylabel('r [m]')
 plt.show()
+# plt.close(fig)
 
-fig = plt.figure()
-plt.plot(rgrid_FF,FField_FF_z[0,0,:].real)
-plt.plot(rgrid_FF,FField_FF_z[0,0,:].imag)
-plt.plot(rgrid_FF,abs(FField_FF_z[0,0,:]))
+# vmin = np.max(np.log(Gaborr))-6.
+fig, ax = plt.subplots()  
+Hankel_errors_logscale = np.log(abs(Hankel_errors[2].T))
+map1 = ax.pcolor(Hgrid_select,rgrid_FF,Hankel_errors_logscale, shading='auto')
+# plt.pcolor(t_Gr,o_Gr/omega0,(np.log(Gaborr)).T, shading='auto',vmin=vmin)
+fig.colorbar(map1)
+# dr_string = ', '+"{:.1f}".format(1e6*rgrid_macro[kr_steps[0]]) +' mum'
+# plt.title('Error' + dr_string)
+plt.xlabel('H [-]')
+plt.ylabel('r [m]')
 plt.show()
+# plt.close(fig)
 
-fig = plt.figure()
-plt.plot(rgrid_FF,FField_FF_z[-1,0,:].real)
-plt.plot(rgrid_FF,FField_FF_z[-1,0,:].imag)
-plt.plot(rgrid_FF,abs(FField_FF_z[-1,0,:]))
-plt.show()
+sys.exit()
 
-fig = plt.figure()
-for k1 in range(35):
-    plt.plot(rgrid_FF,FField_FF_z[k1,0,:].real)
-plt.show()
+# fig = plt.figure()
+# plt.plot(rgrid_FF,abs(FField_FF_z_sum[0]))
+# plt.plot(rgrid_FF,abs(FField_FF_z_sum[1]))
+# plt.plot(rgrid_FF,abs(FField_FF_z_sum[2]))
+# plt.plot(rgrid_FF,abs(FField_FF_z_sum[3]))
+# plt.show()
 
-fig = plt.figure()
-for k1 in range(35):
-    plt.plot(rgrid_FF,FField_FF_z[k1,0,:].imag)
-plt.show()
+# fig = plt.figure()
+# dum = np.max(abs(FField_FF_z_sum[-1]))
+# plt.semilogy(rgrid_FF,abs(FField_FF_z_sum[0]-FField_FF_z_sum[-1])/dum)
+# plt.semilogy(rgrid_FF,abs(FField_FF_z_sum[1]-FField_FF_z_sum[-1])/dum)
+# plt.semilogy(rgrid_FF,abs(FField_FF_z_sum[2]-FField_FF_z_sum[-1])/dum)
+# plt.show()
 
-fig = plt.figure()
-for k1 in range(35):
-    plt.plot(rgrid_FF,np.unwrap(np.angle(FField_FF_z[k1,0,:])))
-plt.show()
+# fig = plt.figure()
+# plt.plot(rgrid_FF,FField_FF_z[0,0,:].real)
+# plt.plot(rgrid_FF,FField_FF_z[0,0,:].imag)
+# plt.plot(rgrid_FF,abs(FField_FF_z[0,0,:]))
+# plt.show()
 
-fig = plt.figure()
-for k1 in range(35):
-    plt.plot(rgrid_FF,abs(FField_FF_z[k1,0,:]))
-plt.show()
+# fig = plt.figure()
+# plt.plot(rgrid_FF,FField_FF_z[-1,0,:].real)
+# plt.plot(rgrid_FF,FField_FF_z[-1,0,:].imag)
+# plt.plot(rgrid_FF,abs(FField_FF_z[-1,0,:]))
+# plt.show()
+
+# fig = plt.figure()
+# for k1 in range(35):
+#     plt.plot(rgrid_FF,FField_FF_z[k1,0,:].real)
+# plt.show()
+
+# fig = plt.figure()
+# for k1 in range(35):
+#     plt.plot(rgrid_FF,FField_FF_z[k1,0,:].imag)
+# plt.show()
+
+# fig = plt.figure()
+# for k1 in range(35):
+#     plt.plot(rgrid_FF,np.unwrap(np.angle(FField_FF_z[k1,0,:])))
+# plt.show()
+
+# fig = plt.figure()
+# for k1 in range(35):
+#     plt.plot(rgrid_FF,abs(FField_FF_z[k1,0,:]))
+# plt.show()
