a55dbb47d083483dadc51461040ebc7b3e01e064
Jan Vabek <vabekjan@gmail.com>
2021-06-30 10:30:34 +0200

    attempt to reshape the plasma to check the speed of the chunking

--- CUPRAD/sources/output.f90
+++ CUPRAD/sources/output.f90
@@ -142,7 +142,7 @@ ENDIF
     ccount = (/int(1,HSIZE_T), int(dim_r_local,HSIZE_T) , int(dim_t,HSIZE_T)/)
 
     dims_shape = (/int(dim_t,HSIZE_T), int(Nz_points,HSIZE_T),int(dim_r,HSIZE_T)/)
-    offset_shape = (int(0,HSIZE_T) /int(output_write_count-1,HSIZE_T),int(dim_r_start(num_proc)-1,HSIZE_T)/)
+    offset_shape = (/int(0,HSIZE_T),int(output_write_count-1,HSIZE_T),int(dim_r_start(num_proc)-1,HSIZE_T)/)
     ccount_shape = (/int(dim_r_local,HSIZE_T) , int(dim_t,HSIZE_T), int(1,HSIZE_T)/)
 
 local_time_MPI  = MPI_Wtime()
