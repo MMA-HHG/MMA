2bf980c37e05abe1fdd9a05278b0a0071b8dddb6
Jan Vabek <vabekjan@gmail.com>
2021-07-14 15:46:48 +0200

    clearing the code, filenames from the input

--- Hankel/Hankel_long_medium.py
+++ Hankel/Hankel_long_medium.py
@@ -16,42 +16,45 @@ import matplotlib.pyplot as plt
 import XUV_refractive_index as XUV_index
 
 
-
+# inputs from hdf5-input
 try:
     with h5py.File('inputs_Hankel.h5', 'r') as Parameters:
         print('reading from hdf5-input file') 
-        gas_type = mn.readscalardataset(Parameters, 'inputs/gas_type', 'S')
-        #  XUV_table_type = mn.readscalardataset(Parameters, 'inputs/XUV_table_type', 'S') # 'NIST' # {Henke, NIST}        
+        file_CUPRAD = mn.readscalardataset(Parameters, 'inputs/file_CUPRAD', 'S')  
+        file_TDSE = mn.readscalardataset(Parameters, 'inputs/file_TDSE', 'S')  
+        out_h5name = mn.readscalardataset(Parameters, 'inputs/out_h5name', 'S')  
+        
+        gas_type = mn.readscalardataset(Parameters, 'inputs/gas_type', 'S')    
         XUV_table_type_diffraction = mn.readscalardataset(Parameters,
                                      'inputs/XUV_table_type_diffraction', 'S')
         XUV_table_type_absorption = mn.readscalardataset(Parameters,
-                                     'inputs/XUV_table_type_absorption', 'S')  
+                                     'inputs/XUV_table_type_absorption', 'S') 
+        apply_diffraction = Parameters['inputs/apply_diffraction'][:].tolist()
+        apply_diffraction = [k1.decode() for k1 in apply_diffraction]
         
-        Nr_max = mn.readscalardataset(Parameters, 'inputs/Nr_max', 'N') # 0.0
-        
-        Nz_max_sum = mn.readscalardataset(Parameters, 'inputs/Nz_max_sum', 'N')
-        
-        Hrange = Parameters['inputs/Hrange'][:].tolist() 
-
         
         kr_step = mn.readscalardataset(Parameters, 'inputs/kr_step', 'N')
-        ko_step = mn.readscalardataset(Parameters, 'inputs/ko_step', 'N')
-        
-        rmax_FF = mn.readscalardataset(Parameters, 'inputs/rmax_FF', 'N')
+        ko_step = mn.readscalardataset(Parameters, 'inputs/ko_step', 'N')       
+        Nr_max = mn.readscalardataset(Parameters, 'inputs/Nr_max', 'N')        
+        Nz_max_sum = mn.readscalardataset(Parameters, 'inputs/Nz_max_sum', 'N')        
+        Hrange = Parameters['inputs/Hrange'][:].tolist()     
+
         Nr_FF = mn.readscalardataset(Parameters, 'inputs/Nr_FF', 'N')
+        rmax_FF = mn.readscalardataset(Parameters, 'inputs/rmax_FF', 'N')
+        
         
         FF_orders_plot = mn.readscalardataset(Parameters, 'inputs/FF_orders_plot', 'N')
         
-        apply_diffraction = Parameters['inputs/apply_diffraction'][:].tolist()
-        apply_diffraction = [k1.decode() for k1 in apply_diffraction]
+
         
 except:
     print('error reading hdf5 file, using defaults') 
     gas_type = 'Kr'
     XUV_table_type_diffraction = 'NIST' # {Henke, NIST}
     XUV_table_type_absorption = 'Henke' # {Henke, NIST} 
-    Nr_max = 235 #470; 235; 155-still fine
+    apply_diffraction = ['dispersion', 'absorption']
     
+    Nr_max = 235 #470; 235; 155-still fine    
     Hrange = [17, 18] # [17, 18] # [14, 36] [17, 18] [16, 20] [14, 22]
     
     kr_step = 2 # descending order, tha last is "the most accurate"
@@ -60,26 +63,12 @@ except:
     rmax_FF = 8*1e-4
     Nr_FF = 200
     
-    FF_orders_plot = 4
-    
+    FF_orders_plot = 4    
     Nz_max_sum = 5 # 41
-
     
-    
-
-# gas_type = 'Kr'
-# XUV_table_type_diffraction = 'NIST' # {Henke, NIST}
-# XUV_table_type_absorption = 'Henke' # {Henke, NIST}
-
-
-def f1_funct(E):
-    return XUV_index.getf1(gas_type+'_' + XUV_table_type_diffraction, E)
-
-def f2_funct(E):
-    return XUV_index.getf2(gas_type + '_' + XUV_table_type_absorption, E)
-
-# f2 = XUV_index.getf2(gas_type+'_'+XUV_table_type, mn.ConvertPhoton(50e-9, 'lambdaSI', 'eV'))
-# f1f2 = XUV_index.getf(gas_type+'_'+XUV_table_type, mn.ConvertPhoton(20e-9, 'lambdaSI', 'eV'))
+    file_CUPRAD = 'results_1.h5'
+    file_TDSE = 'results_merged_Fsource.h5'
+    out_h5name = 'Hankel.h5'
 
 
 arguments = sys.argv
@@ -94,24 +83,19 @@ else:
     results_CUPRAD = os.path.join("D:\data", "Discharges", "TDSE", "t6")
     results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSEH1")
     results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSE40planes1")
-    results_TDSE = os.path.join("C:\data", "Discharges", "TDSE", "TDSE50planes1") # fine up to 46
+    results_TDSE = os.path.join("C:\data", "Discharges", "TDSE", "TDSE50planes1") # fine up to 46  
     
-    # results_TDSE = os.path.join("D:\data", "Discharges", "TDSE", "TDSE10planes1")
 
 
-file_CUPRAD = 'results_1.h5'
-file_TDSE = 'results_merged_Fsource.h5' # 'hdf5_temp_0000000.h5'
 
 
 file_CUPRAD = os.path.join(results_CUPRAD,file_CUPRAD)
 file_TDSE = os.path.join(results_TDSE,file_TDSE)
 
 
-# sys.exit()
-
+# load data
 print('processing:', file_CUPRAD, file_TDSE)             
 with h5py.File(file_CUPRAD, 'r') as InputArchiveCUPRAD, h5py.File(file_TDSE, 'r') as InputArchiveTDSE:
-   # load data
    omega0 = mn.ConvertPhoton(1e-2*mn.readscalardataset(InputArchiveCUPRAD,'/inputs/laser_wavelength','N'),'lambdaSI','omegaau')
    inverse_GV_IR = InputArchiveCUPRAD['/logs/inverse_group_velocity_SI'][()]; group_velocity_IR = 1./inverse_GV_IR
    # pressure_mbar = 1e3*InputArchiveCUPRAD['/inputs/medium_pressure_in_bar'][()]
@@ -123,22 +107,17 @@ with h5py.File(file_CUPRAD, 'r') as InputArchiveCUPRAD, h5py.File(file_TDSE, 'r'
    rgrid_macro = InputArchiveTDSE['rgrid_coarse'][:]
    zgrid_macro = InputArchiveTDSE['zgrid_coarse'][:]
 
-   
-   # GS_init = InputArchiveTDSE['ground_state'][:,0] + 1j*InputArchiveTDSE['ground_state'][:,1]
 print('data loaded:')
 omega_au2SI = mn.ConvertPhoton(1.0, 'omegaau', 'omegaSI')
 ogridSI = omega_au2SI * ogrid
 Hgrid = ogrid/omega0
 H_indices = [mn.FindInterval(Hgrid,Hvalue) for Hvalue in Hrange]
 
-out_h5name = 'Hankel.h5'
-
 try:
     os.remove(out_h5name)
     print("previous results deleted")
 except:
-    print("no files deleted")
-    
+    print("no files deleted")  
 
 rgrid_FF = np.linspace(0.0, rmax_FF, Nr_FF)
 ogrid_select_SI = ogridSI[H_indices[0]:H_indices[1]:ko_step]
@@ -146,6 +125,12 @@ ogrid_select_SI = ogridSI[H_indices[0]:H_indices[1]:ko_step]
 
 
 # Here are the fuction to obtain the phase factors in SI units: exp(i*omega*function(omega))
+def f1_funct(E):
+    return XUV_index.getf1(gas_type+'_' + XUV_table_type_diffraction, E)
+def f2_funct(E):
+    return XUV_index.getf2(gas_type + '_' + XUV_table_type_absorption, E)
+
+
 def dispersion_function_def(omega):
     f1_value = f1_funct(mn.ConvertPhoton(omega, 'omegaSI', 'eV'))    
     lambdaSI = mn.ConvertPhoton(omega, 'omegaSI', 'lambdaSI')
@@ -168,6 +153,7 @@ if ('absorption' in apply_diffraction): absorption_function = absorption_functio
 else: dispersion_function = None
 
 
+# The main integration
 FField_FF_integrated = Hfn2.HankelTransform_long(ogrid_select_SI,
                                                rgrid_macro[0:Nr_max:kr_step],
                                                zgrid_macro[:Nz_max_sum],
@@ -178,39 +164,13 @@ FField_FF_integrated = Hfn2.HankelTransform_long(ogrid_select_SI,
                                                absorption_function = absorption_function)
 
 
- # FSourceTerm_select = np.squeeze(FSourceTerm[0:Nr_max:kr_step,k1,H_indices[0]:H_indices[1]:ko_step]).T
-         
- #    # FSourceTerm_select[np.newaxis] # to enforce correct dimensions
- #    # FSourceTerm_select.reshape((1,) + FSourceTerm_select.shape) # to enforce correct dimensions
-    
-   
-    
- #    FField_FF = Hfn2.HankelTransform(ogrid_select_SI,
- #                                      rgrid_macro[0:Nr_max:kr_step],
- #                                      FSourceTerm_select,
- #                                      0.3-zgrid_macro[k1],
- #                                      rgrid_FF)
-
-
+# Save the data
 with h5py.File(out_h5name,'w') as OutFile:
     grp = OutFile.create_group('XUV')
     grp.create_dataset('Spectrum_on_screen',
                                           data = np.stack((FField_FF_integrated.real, FField_FF_integrated.imag),axis=-1)
                                           )
-    # grp.create_dataset('Spectrum_on_screen_abs',
-    #                    data = np.stack((FField_FF_int.real, FField_FF_int.imag),axis=-1)
-    #                    )
-    
-    # grp.create_dataset('Spectrum_on_screen_disp',
-    #                    data = np.stack((FField_FF_int_adj_test.real, FField_FF_int_adj_test.imag),axis=-1)
-    #                    ) 
 
-    # grp.create_dataset('Spectrum_on_screen_disp+abs',
-    #                    data = np.stack((FField_FF_int_adj_abs_test.real, FField_FF_int_adj_abs_test.imag),axis=-1)
-    #                    )   
-    # grp.create_dataset('Spectrum_on_screen_test',
-    #                                       data = np.stack((FField_FF_int_test.real, FField_FF_int_test.imag),axis=-1)
-    #                                       )    
 
 Hgrid_select = Hgrid[H_indices[0]:H_indices[1]:ko_step]
 
@@ -227,47 +187,3 @@ plt.ylabel('r [m]')
 if showplots: plt.show()
 # plt.close(fig)
 # sys.exit()
-
-
-# # vmin = np.max(np.log(Gaborr))-6.
-# fig, ax = plt.subplots()   
-# FF_spectrum_logscale = np.log10(abs(FField_FF_int_adj_test.T)**2);
-# vmin = np.max(FF_spectrum_logscale)-FF_orders_plot
-# map1 = ax.pcolor(Hgrid_select,rgrid_FF,FF_spectrum_logscale, shading='auto',vmin=vmin)
-# # plt.pcolor(t_Gr,o_Gr/omega0,(np.log(Gaborr)).T, shading='auto',vmin=vmin)
-# fig.colorbar(map1)
-# plt.title('Far-field spectrum (30 cm), integrated, dispersion, log')
-# plt.xlabel('H [-]')
-# plt.ylabel('r [m]')
-# if showplots: plt.show()
-# # plt.close(fig)
-# # sys.exit()
-
-
-# # vmin = np.max(np.log(Gaborr))-6.
-# fig, ax = plt.subplots()   
-# FF_spectrum_logscale = np.log10(abs(FField_FF_int_adj_abs_test.T)**2);
-# vmin = np.max(FF_spectrum_logscale)-FF_orders_plot
-# map1 = ax.pcolor(Hgrid_select,rgrid_FF,FF_spectrum_logscale, shading='auto',vmin=vmin)
-# # plt.pcolor(t_Gr,o_Gr/omega0,(np.log(Gaborr)).T, shading='auto',vmin=vmin)
-# fig.colorbar(map1)
-# plt.title('Far-field spectrum (30 cm), integrated, disp + abs, log')
-# plt.xlabel('H [-]')
-# plt.ylabel('r [m]')
-# if showplots: plt.show()
-# # plt.close(fig)
-# # sys.exit()
-
-# # vmin = np.max(np.log(Gaborr))-6.
-# fig, ax = plt.subplots()   
-# FF_spectrum_logscale = np.log10(abs(FField_FF_int_adj_abs_test.T)**2);
-# vmin = np.max(FF_spectrum_logscale)-FF_orders_plot
-# map1 = ax.pcolor(Hgrid_select,rgrid_FF,FF_spectrum_logscale, shading='auto',vmin=vmin)
-# # plt.pcolor(t_Gr,o_Gr/omega0,(np.log(Gaborr)).T, shading='auto',vmin=vmin)
-# fig.colorbar(map1)
-# plt.title('Far-field spectrum (30 cm), integrated, disp + abs, log')
-# plt.xlabel('H [-]')
-# plt.ylabel('r [m]')
-# if showplots: plt.show()
-# # plt.close(fig)
-# # sys.exit()
