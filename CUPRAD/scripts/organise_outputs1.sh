0ca88ebe2088a66e90cf33c3f57079f2cc8ad6bf
Jan Vabek <vabekjan@gmail.com>
2021-09-14 20:12:09 +0200

    update move script

---
+++ CUPRAD/scripts/organise_outputs1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+mkdir dPhi_maps
+mv abs_dPhi_dz_* dPhi_maps/
+mv dPhi_dz_* dPhi_maps/
+
+mkdir Curvature
+mv Curvature_* Curvature/
+mv ddr_Curvature_* Curvature/
+
+mkdir Cutoffs_slices
+mv Cutoff_t* Cutoffs/
+
+mkdir Fluences_and_max_cutoffs
+mv Fluence_* Fluences_and_max_cutoffs/
+mv Cutoff_max_* Fluences_and_max_cutoffs/
+
+mkdir Intensity
+mv Intens_* Intensity/
+
+mkdir Efield
+mv Efield_* Efield/
+
+mkdir Lcoh
+mv Lcoh_* Lcoh/
+
+mkdir Plasma
+mv Plasma_* Plasma/
+
+mkdir phases_onax
+mv phase_onax_* phases_onax/
+
+mkdir misc
+mv phase_in_* misc/
+
+echo "done" 
