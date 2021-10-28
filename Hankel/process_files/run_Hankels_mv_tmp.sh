2c9fc5db97c1626b484e924d9fcab42bdc717cb4
Jan Vabek <vabekjan@gmail.com>
2021-10-28 11:03:01 +0200

    script to run Hankels and mv tmp

---
+++ Hankel/process_files/run_Hankels_mv_tmp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 $UNIV_INPUT_PATH/create_universal_HDF5.py -i $HANKEL_HOME/process_files/Hankel_Inputs_mp_large_all_cummulative.inp -ohdf5 inputs_Hankel_all_cummulative.h5 -g inputs
+
+python3 $UNIV_INPUT_PATH/create_universal_HDF5.py -i $HANKEL_HOME/process_files/Hankel_Inputs_mp_large_all_cummulative_noabs.inp -ohdf5 inputs_Hankel_all_cummulative_noabs.h5 -g inputs
+
+sbatch $HANKEL_HOME/process_files/Hankel_mp_all_cummulative.slurm
+sbatch $HANKEL_HOME/process_files/Hankel_mp_all_cummulative_noabs.slurm
+
+mkdir temp
+mv hdf5_temp_000* temp/
