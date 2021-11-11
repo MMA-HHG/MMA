9ed063ab104abeb15ceca109d2f3ab02b14b761c
Jan Vabek <vabekjan@gmail.com>
2021-11-11 16:45:06 +0100

    collect Hankel develop

--- Hankel/collect_Hankels.py
+++ Hankel/collect_Hankels.py
@@ -20,6 +20,8 @@ for folder in folders:
     # path = os.path.join(folder, 'TDSEs')
     Hankels = glob.glob(os.path.join(folder, 'TDSEs','Hankel_all*.h5'))
     print(Hankels)
+    for Hankel in Hankels:
+        print(os.path.basename(Hankel))
 
 # for fname in files:
 #     with h5py.File(fname,'r') as inpf, h5py.File(os.path.join(results_path,fname),'w') as outf:
