b1e50fa526dde172a948e1dc49a3097c55cf6141
Jan Vabek <vabekjan@gmail.com>
2021-11-12 18:21:59 +0100

    analyses of the TDSE pre-ionised

---
+++ CUPRAD/python/single_file_end_plasma.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+import time
+import shutil
+import h5py
+import sys
+import units
+import mynumerics as mn
+import HHG
+import matplotlib.pyplot as plt
+import re
+import glob
+import XUV_refractive_index as XUV_index
+import IR_refractive_index as IR_index
+from contextlib import ExitStack
+import dataformat_CUPRAD as dfC
