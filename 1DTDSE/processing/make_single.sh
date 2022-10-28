e537615ab55c9bf752ba12a3077d7a20bb98f136
Jan Vabek <vabekjan@gmail.com>
2022-10-28 19:25:15 +0200

    typo

--- 1DTDSE/processing/make_single.sh
+++ 1DTDSE/processing/make_single.sh
@@ -3,4 +3,4 @@
 
 python3 $TDSE_1D_HOME/prepare_fields/prepare_field_single.py -nodisplay
 
-python3 $UNIV_INPUT_PATH/create_universal_HDF5.py -i $TDSE_1D_HOME/processing/FreeFormInputs_TDSE_single.inp -ihdf5 results.h5 -ohdf5 results.h5 -g TDSE_inputs 
+python3 $UNIV_INPUT_PATH/create_universal_HDF5.py -i $TDSE_1D_HOME/processing/FreeFormInputs_TDSE_single.inp -ihdf5 field_input.h5 -ohdf5 results.h5 -g TDSE_inputs 
