b59637411f7ac73d6291ead8ffd6c7b5285918a6
Jan Vabek <vabekjan@gmail.com>
2022-11-02 18:34:02 +0100

    db

--- 1DTDSE/processing/make_par.sh
+++ 1DTDSE/processing/make_par.sh
@@ -3,6 +3,6 @@
 
 python3 $TDSE_1D_HOME/prepare_fields/prepare_fields.py -nodisplay -g fields_list -ohdf5 fields_list.h5 -g_params grids_for_scans -i $TDSE_1D_HOME/prepare_fields/TDSE_create_fields.inp
 
-python3 $UNIV_INPUT_PATH/create_universal_HDF5.py -i $TDSE_1D_HOME/processing/FreeFormInputs_TDSE_par.inp -ihdf5 fields_list.h5 -ohdf5 results.h5 -g TDSE_inputs 
+python3 $UNIV_INPUT_PATH/create_universal_HDF5.py -i $TDSE_1D_HOME/processing/FreeFormInputs_TDSE_par1.inp -ihdf5 fields_list.h5 -ohdf5 results.h5 -g TDSE_inputs 
 
 cp results.h5 results2.h5
