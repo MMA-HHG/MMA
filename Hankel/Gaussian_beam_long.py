14bf9459165a6b0b512cae1d7f97999e6db716a7
Jan Vabek <vabekjan@gmail.com>
2023-03-16 22:13:17 +0100

    experimenting with phase factors

--- Hankel/Gaussian_beam_long.py
+++ Hankel/Gaussian_beam_long.py
@@ -119,6 +119,7 @@ domega = ogrid[1]-ogrid[0]
 dE0 = E0_grid[1]-E0_grid[0]
 k_omega_sel = list(mn.FindInterval(Hgrid,Hlimit))
 ogrid_sel = ogrid[k_omega_sel[0]:k_omega_sel[1]]
+ogrid_sel_SI = mn.ConvertPhoton(1.0, 'omegaau', 'omegaSI')*ogrid_sel
 No_sel = len(ogrid_sel)
 
 Hgrid_sel = ogrid_sel/omega0
@@ -286,6 +287,8 @@ Fsource_long_prof =  FSourceTerm_interpE0( E0_sel *
                                                           incl_radial_wz_profile = True))
 
 
+
+
 image = pp.figure_driver()    
 image.sf = [pp.plotter() for k2 in range(16)]
 image.title = 'profile'
@@ -308,6 +311,72 @@ image.sf = [pp.plotter() for k2 in range(16)]
 image.title = 'sources phase'
 image.sf[0].args = [HHG.ComputeCutoff(E0_grid**2, omega0, Ip_TDSE_au)[1], np.unwrap(np.angle(FSourceTerm[:,mn.FindInterval(Hgrid, 17)])) ]
 pp.plot_preset(image)
+
+
+
+
+## signal including IR phase from the phase factor
+dispersion_function = dispersion_function_disp_def
+No = len(ogrid_sel)
+dispersion_factor = np.empty(No)
+for k1 in range(No):
+    dispersion_factor[k1] = ogrid_sel_SI[k1]*dispersion_function(ogrid_sel_SI[k1])   
+    # dispersion_factor[k1] = np.pi/2.1777380065358176e-07 # ogrid[k1]*dispersion_function(ogrid[k1])   
+
+factor_e1 = np.exp(1j*np.outer(zgr,dispersion_factor))
+
+
+Fsource_long_prof_disp1 =  FSourceTerm_interpE0( E0_sel * 
+                                          Gaussian_E0_map(zgr,np.asarray([0]),w0,1.0,
+                                                          mn.ConvertPhoton(omega0SI, 'omegaSI', 'lambdaSI'),
+                                                          n=n_IR,
+                                                          incl_z_profile = True,
+                                                          incl_radial_wz_profile = True))
+
+sig_long_prof_disp1 = pressure*Signal_cum_integrator(ogrid_sel, zgr, (factor_e1 * Fsource_long_prof_disp1).T)
+
+
+## signal including IR phase from the Gaussian beam constructor
+dispersion_function = dispersion_function_vac_def
+No = len(ogrid_sel)
+dispersion_factor = np.empty(No)
+for k1 in range(No):
+    dispersion_factor[k1] = ogrid_sel_SI[k1]*dispersion_function(ogrid_sel_SI[k1])   
+    # dispersion_factor[k1] = np.pi/2.1777380065358176e-07 # ogrid[k1]*dispersion_function(ogrid[k1])   
+
+factor_e_vac = np.exp(1j*np.outer(zgr,dispersion_factor))
+
+
+Fsource_long_prof_disp2 =  FSourceTerm_interpE0( E0_sel * 
+                                          Gaussian_E0_map(zgr,np.asarray([0]),w0,1.0,
+                                                          mn.ConvertPhoton(omega0SI, 'omegaSI', 'lambdaSI'),
+                                                          n=n_IR,
+                                                          incl_z_profile = True,
+                                                          incl_radial_wz_profile = True))
+
+factor_e_Gauss = np.exp(1j*np.outer((ogrid_sel_SI/omega0SI),
+                                    Gaussian_phase_map(
+                                        zgr,0,w0,mn.ConvertPhoton(omega0SI, 'omegaSI', 'lambdaSI'),
+                                        n=n_IR,
+                                        vacuum_frame=True, incl_curv = False, incl_Gouy = False, incl_lin = True)
+                                    ).T
+                        )
+
+factor_e2 = factor_e_vac * factor_e_Gauss
+
+sig_long_prof_disp2 = pressure*Signal_cum_integrator(ogrid_sel, zgr, (factor_e2 * Fsource_long_prof_disp2).T)
+
+
+
+## compare phases
+k_Hsel = mn.FindInterval(Hgrid_sel, 17)
+image = pp.figure_driver()    
+image.sf = [pp.plotter() for k2 in range(16)]
+image.title = 'full model phases'
+image.sf[0].args = [zgr,np.angle(factor_e1.T[k_Hsel,:])]
+image.sf[1].args = [zgr,np.angle(factor_e2.T[k_Hsel,:]), '--']
+pp.plot_preset(image)
+
 
 
 
@@ -356,6 +425,48 @@ image.sf = [pp.plotter() for k2 in range(16)]
 image.title = 'from TDSE phase'
 image.sf[0].args = [zgr,np.abs(sig_long_prof_phase[k_Hsel,:])**2]
 pp.plot_preset(image)
+
+
+image = pp.figure_driver()    
+image.sf = [pp.plotter() for k2 in range(16)]
+image.title = 'from TDSE incl. all'
+# image.sf[0].args = [zgr,np.abs(sig_long[k_Hsel,:])**2]
+# image.sf[1].args = [zgr,np.abs(sig_long_prof[k_Hsel,:])**2]
+image.sf[2].args = [zgr,np.abs(sig_long_prof_disp1[k_Hsel,:])**2, '-']
+image.sf[3].args = [zgr,np.abs(sig_long_prof_disp2[k_Hsel,:])**2, '-.']
+pp.plot_preset(image)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
