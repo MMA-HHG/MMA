f234d872073c6630296190d30c8a5c4fe988ee66
nemectad <tada.nemec@seznam.cz>
2023-08-22 14:12:16 +0200

    Help script for exporting env variables

---
+++ set_env_vars.sh
@@ -0,0 +1,42 @@
+### This shell scripts sets all the necessary env variables
+
+### To run this script make sure you made the script executable using ```chmod``` 
+### command and run with the command ```source ./set_env_vars.sh``` or simply as
+### ```. ./set_env_vars.sh```.
+
+### Default path with directories - should contain all the necessary git repos
+export GIT_PATH=/Users/tadeasnemec/Programming/Git
+
+export UNIV_INPUT_PATH=$GIT_PATH/universal_input
+
+export MSM_PATH=$GIT_PATH/CUPRAD_TDSE_Hankel
+
+export PYTHONPATH=$PYTHONPATH:$GIT_PATH/python_modules:$MSM_PATH/shared_python
+
+export CUPRAD_HOME=$MSM_PATH/CUPRAD
+
+export CUPRAD_BINARY=$MSM_PATH/CUPRAD/binary
+
+export CUPRAD_SCRIPTS=$MSM_PATH/CUPRAD/scripts
+
+export CUPRAD_INPUTS=$MSM_PATH/CUPRAD/testing
+
+export CUPRAD_PYTHON=$MSM_PATH/CUPRAD/python
+
+export TDSE_1D_SOURCE=$MSM_PATH/1DTDSE/
+
+export TDSE_1D_POST_PROCESSING=$MSM_PATH/1DTDSE/post_processing
+
+export TDSE_1D_SCRIPTS=$MSM_PATH/1DTDSE/scripts
+
+export TDSE_1D_SLURM=$MSM_PATH/1DTDSE/slurm
+
+export TDSE_1D_BINARY=$MSM_PATH/1DTDSE/binary
+
+export HANKEL_HOME=$MSM_PATH/Hankel
+
+export TDSE_1D_HOME=$MSM_PATH/1DTDSE
+
+export MULTISCALE_HOME=$MSM_PATH
+
+export MULTISCALE_SCRIPTS=$MSM_PATH/multiscale/scripts
