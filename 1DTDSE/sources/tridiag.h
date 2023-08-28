443ffbcb0ca823e4aa15c87d6666fe2dbb5b8367
nemectad <tada.nemec@seznam.cz>
2023-08-28 13:56:35 +0200

    Clean tridiagonal methods.

--- 1DTDSE/sources/tridiag.h
+++ 1DTDSE/sources/tridiag.h
@@ -14,15 +14,9 @@
 #include <stdio.h>
 #include "structures.h"
 
-int QL_Tridiagonal_Symmetric_Matrix( double *, double *,double *, int, int);
-void Inv_Tridiagonal_Matrix( double *, double *, double *, double *, double *, int);
 void Inv_Tridiagonal_Matrix_complex( double *, double *, double *, double *, double *, int );
 void Inv_Tridiagonal_Matrix_complex_Numerov( double *, double *, double *, double *, double*, int );
 double Einitialise( trg_def, double *,double *,double *,double *,double *,double,double ,int);
-double E_calculation(double *,double *,double *,double *,int);
 double E_calculation_numerov( trg_def, double *,double ,double *,int );
-double Calculate_Shift(double, double, double); 
-void Transform_Matrix(double*, double, double, int, int);
-
 
 #endif
