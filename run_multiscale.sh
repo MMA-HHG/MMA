c677975e88b70795383a5423faf8ca24d6e53583
nemectad <tada.nemec@seznam.cz>
2023-12-08 13:29:49 +0100

    MPI-TDSE and HDF5 merge added to MSM pipeline

--- run_multiscale.sh
+++ run_multiscale.sh
@@ -7,14 +7,22 @@ usage()
 ----------------------------------------------------
 usage: [[[-i (--inp) input file with CUPRAD parameters (.inp)] 
          [-o (--ohdf5) output HDF5 file] 
-         [-n (--ncuprad) number of processes for CUPRAD execution (must fit input file!)]] | 
+         [-n (--ncuprad) number of processes for CUPRAD execution (must fit input file!)] 
+         [-m (--ntdse) number of processes for MPI-TDSE execution]  
+         [-p (--printdata) data to print into the merged HDF5 file]] | 
          [-h help]]
 
 ----------------------------------------------------
-run: source /path/to/script/../run_multiscale.sh -i [input_file.inp] -o [result.h5] -n [32]
-
 In order to run the script properly, environment variables must be set using 'set_env_vars.sh' 
 script in the current terminal shell as we execute the 'run_multiscale.sh' script.
+
+Example run: source /path/to/script/../run_multiscale.sh -i [input_file.inp] -o [result.h5] -n [N_tasks_cuprad] -m [M_tasks_tdse] -p ['Efield FSourceTerm SourceTerm expval_x PopInt PopTot ...']
+
+
+Print data available options: 'Efield', 'FEfield', 'SourceTerm', 'FSourceTerm', 
+                              'FEfieldM2', 'FSourceTermM2', 'PopTot', 'PopInt', 
+                              'expval_x'
+Note: to use this option, the keywords must be in parentheses, e.g. "Efield SourceTerm".
 "
 }
 
@@ -29,6 +37,12 @@ while [ "$1" != "" ]; do
                                 ;;
         -n | --ncuprad)         shift
                                 ntasks_cuprad="$1"
+                                ;;
+        -m | --ntdse)           shift
+                                ntasks_tdse="$1"
+                                ;;
+        -p | --printdata        shift
+                                printdata="$1"
                                 ;;
         -h | --help )           usage
                                 exit
@@ -57,9 +71,16 @@ then
     exit 1
 fi
 
+if [ "$ntasks_tdse" = "" ]
+then
+    echo "Unspecified number of processes for MPI-TDSE. Use option [-m (--ntdse)]."
+    exit 1
+fi
+
 echo "Input file: ${inp_filename}"
 echo "Output hdf5 file: ${h5_filename}"
 echo "Number of processes for CUPRAD: ${ntasks_cuprad}"
+echo "Number of processes for MPI-TDSE: ${ntasks_tdse}"
 
 ### ENVIRONMENT VARIABLES MUST BE SET!
 #source set_env_vars.sh
@@ -75,3 +96,11 @@ JOB1=$(sbatch --parsable --export=ALL --dependency=afterok:$JOB0 \
 ### Submit CUPRAD
 JOB2=$(sbatch --ntasks=$ntasks_cuprad --parsable --export=ALL --dependency=afterok:$JOB1 \
         $CUPRAD_SCRIPTS/CUPRAD.sh)
+
+### Submit TDSE
+JOB3=$(sbatch --ntasks=$ntasks_tdse --parsable --export=ALL --dependency=afterok:$JOB2 \
+        $TDSE_1D_SCRIPTS/MPI_TDSE.sh)
+
+### Merge TDSEs
+JOB4=$(sbatch --parsable --export=ALL --dependency=afterok:$JOB3 \
+        $TDSE_1D_SCRIPTS/merge_hdf5.sh -p $printdata -s)
