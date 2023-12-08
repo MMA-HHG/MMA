c10cc8cb40b54a19d53b8711c1c39bf11c6400c2
nemectad <tada.nemec@seznam.cz>
2023-12-08 11:02:50 +0100

    Module loading via bash func fix

--- Modules/load_modules.sh
+++ Modules/load_modules.sh
@@ -2,7 +2,7 @@
 
 ### Define your modules and HPCs here
 ### Define separately modules for compiling and modules for Python
-function Curta() {
+Curta_modules() {
     module load cmake/3.26.3/gcc@11.2.0-rthuom3
     module load compiler/intel/2020.4.304
     module load mpi/intel/2020.4.304
@@ -10,25 +10,31 @@ function Curta() {
     module load hdf5/1.10.5/openmpi_4.1.2/intel_2022.0.1
     module load hdf5/1.10.5/impi_2019.1.144/intel_2019.1.144
 }
+export -f Curta_modules
 
-function Curta_python() {
+Curta_python_modules() {
     module load python/3.9
 }
-
+export -f Curta_python_modules
 
 ### Load cmake, MPI, HDF5, FFTW/MKL
-function load_modules() {
-    if ["$HPC" = "Curta"] 
+load_modules() {
+    if [ "$HPC" == "Curta" ] 
     then
-        Curta
+        Curta_modules
     fi
 
 }
+export -f load_modules
 
-function load_python_modules() {
+load_python_modules() {
     if ["$HPC" = "Curta"] 
     then
-        Curta_python
+        Curta_python_modules
     fi
 
 }
+export -f load_python_modules
+
+echo "Functions loaded into env."
+echo "HPC = $HPC"
