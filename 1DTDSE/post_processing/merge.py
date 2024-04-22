70f5b378e4b5dc3d5990e753b568eac85fa6a8cc
Jan Vabek <vabekjan@gmail.com>
2024-04-22 19:02:39 +0200

    CTDSE merge: h5-paths

--- 1DTDSE/post_processing/merge.py
+++ 1DTDSE/post_processing/merge.py
@@ -10,35 +10,44 @@
 # finding mn from external location
 # https://stackoverflow.com/questions/17198319/how-to-configure-custom-pythonpath-with-vm-and-pycharm
 
+import sys
+import os
 import numpy as np
 import h5py
 import mynumerics as mn
 import glob
-import argparse
+# import argparse
 import MMA_administration as MMA
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--printdata", nargs='+', required=False, help="Select data to print. Available " \
-                "options are 'Efield', 'FEfield', 'SourceTerm', 'FSourceTerm', 'FEfieldM2', " \
-                "'FSourceTermM2', 'PopTot', " \
-                "'PopInt', 'expval_x'")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-p", "--printdata", nargs='+', required=False, help="Select data to print. Available " \
+#                 "options are 'Efield', 'FEfield', 'SourceTerm', 'FSourceTerm', 'FEfieldM2', " \
+#                 "'FSourceTermM2', 'PopTot', " \
+#                 "'PopInt', 'expval_x'")
+# args = vars(ap.parse_args())
+
+arguments = sys.argv
+
+if not(os.path.isfile('msg.tmp')): raise FileNotFoundError("'msg.tmp' required to pass the name off the archive.")
+with 'msg.tmp' as f: outfname = f.readline().rstrip()  # "results_merged.h5"
+
+h5path = MMA.paths['CTDSE_outputs']
 
 available_outputs_list = ['Efield', 'FEfield', 'SourceTerm', 'FSourceTerm', 'FEfieldM2', 'FSourceTermM2', 'PopTot',
                           'PopInt', 'expval_x'] 
 
-if args['printdata'] != None: 
-    args = args['printdata']
-    available_outputs_list = set(args).intersection(available_outputs_list)
-    if available_outputs_list == set():
-        available_outputs_list = ['Efield', 'FEfield', 'SourceTerm', 'FSourceTerm', 'FEfieldM2', 'FSourceTermM2', 'PopTot',
-                          'PopInt', 'expval_x'] 
-    print("Datasets to be printed: ")
-    print(available_outputs_list)
+# if args['printdata'] != None: 
+#     args = args['printdata']
+#     available_outputs_list = set(args).intersection(available_outputs_list)
+#     if available_outputs_list == set():
+#         available_outputs_list = ['Efield', 'FEfield', 'SourceTerm', 'FSourceTerm', 'FEfieldM2', 'FSourceTermM2', 'PopTot',
+#                           'PopInt', 'expval_x'] 
+#     print("Datasets to be printed: ")
+#     print(available_outputs_list)
 
 
 files = glob.glob('hdf5_temp_*.h5') # filter all the single-proc files
-outfname = "results_merged.h5"
+# outfname = "results_merged.h5"
 available_further_data = ['tgrid', 'omegagrid', 'Energy_of_the_ground_state', 'xgrid_micro', 'ground_state', 'zgrid_coarse',
                           'rgrid_coarse']
 precision = 'd'
@@ -50,10 +59,10 @@ def prepare_ouput_file(f,outf,dset_list):
         if (dsetname in available_outputs_list):
             dset = f[dsetname]
             newshape =  joint_rz_shape + dset.shape[0:-1]
-            newdset = outf.create_dataset(dsetname, newshape,precision)
+            newdset = outf.create_dataset(h5path+dsetname, newshape,precision)
 
         if (dsetname in available_further_data):
-            f.copy(dsetname,outf)
+            f.copy(dsetname,outf[h5path])
 
 
 def print_ouput_file(f,outf,dset_list):
@@ -65,7 +74,7 @@ def print_ouput_file(f,outf,dset_list):
         if (dsetname in available_outputs_list):
             dset = f[dsetname]
             ndims = len(dset.shape)
-            newdset = outf[dsetname]
+            newdset = outf[h5path+dsetname]
             data = dset[()]
             for k1 in range(nsim_loc):
                 if (ks_loc[k1] >= 0): # only for used keys
