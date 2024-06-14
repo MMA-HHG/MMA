43cd6dfd3cb670e93bbb0d2cf77bce99e5ae4c5f
Jan Vabek <vabekjan@gmail.com>
2024-06-14 18:43:58 +0200

    designing slurm pipeline for the whole model

---
+++ multiscale/scripts/run_multiscale_SUNRISE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Submit the pre-processor
+JOB1=$(sbatch --parsable $MULTISCALE_SCRIPTS/slurm/CUPRAD_pre_processor.slurm)
+# touch 1.test
+
+# Submit the main job when the pre-processor is finished
+JOB2=$(sbatch --parsable --dependency=afterok:$JOB1 $MULTISCALE_SCRIPTS/slurm/CUPRAD_SUNRISE16MIX.slurm)
+
+JOB3=$(sbatch --parsable --dependency=afterok:$JOB2 $MULTISCALE_SCRIPTS/slurm/CTDSE_prepare_MPI.slurm)
+
+# Run TDSE
+JOB4=$(sbatch --parsable --dependency=afterok:$JOB3 $MULTISCALE_SCRIPTS/slurm/MPI_CTDSE_SUNRISE.slurm)
+
+# Collect & merge data
+JOB5=$(sbatch --parsable --dependency=afterok:$JOB4 $MULTISCALE_SCRIPTS/slurm/CTDSE_merge_hdf5.slurm)
+
+# JOB6=$(sbatch --dependency=afterok:$JOB5 $TDSE_1D_SCRIPTS/remove_temp.slurm)
+
+
+
+JOBH1=$(sbatch --dependency=afterok:$JOB5 $MULTISCALE_SCRIPTS/slurm/process_files/Hankel_mp_all_cummulative.slurm)
