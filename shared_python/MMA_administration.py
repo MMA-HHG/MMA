726c39d01e36a4fb3297de970bacfacdab5b792c
Jan Vabek <vabekjan@gmail.com>
2024-08-08 14:41:40 +0200

    upadate MMA paths & procedures to create code inputs

--- shared_python/MMA_administration.py
+++ shared_python/MMA_administration.py
@@ -39,3 +39,39 @@ def pressure_constructor(h5_handle):
         pressure = pressure_base
         
     return pressure
+
+
+global_variable_type_lists ={
+    'S' : ['gas_preset'],
+    'R' : ['medium_pressure_in_bar'],
+    'I' : [], 'R-array' : []}
+
+CUPRAD_variable_type_lists ={
+    'I' : ['numerics_number_of_points_in_r', 'numerics_number_of_points_in_t',
+           'numerics_operators_t_t-1'],
+    'R' : ['laser_wavelength', 'laser_pulse_duration_in_1_e_Efield',
+           'laser_focus_intensity_Gaussian', 'laser_focus_beamwaist_Gaussian',
+           'laser_focus_position_Gaussian', 'medium_physical_distance_of_propagation',
+           'numerics_physical_first_stepwidth', 'numerics_phase_threshold_for_decreasing_delta_z',
+           'numerics_length_of_window_for_r_normalized_to_beamwaist',
+           'numerics_length_of_window_for_t_normalized_to_pulse_duration',
+           'numerics_number_of_absorber_points_in_time', 'numerics_physical_output_distance_for_plasma_and_Efield',
+           'numerics_output_distance_in_z-steps_for_fluence_and_power',
+           'numerics_radius_for_diagnostics', 'numerics_run_time_in_hours'],
+    'S' : ['ionization_model'],
+    'R-array': []}
+
+CTDSE_variable_type_lists ={
+    'I' : ['Nr_max', 'kr_step', 'kz_step', 'Nx_max']+
+          ['print_'+foo for foo in ('GS', 'Efield', 'F_Efield', 'Source_Term', 
+                                    'F_Source_Term', 'GS_population',
+                                    'integrated_population', 'x_expectation_value')],
+    'R' : ['x_int', 'dx', 'dt', 'CV_criterion_of_GS'],
+    'S' : [], 'R-array' : []}
+
+Hankel_variable_type_lists ={
+    'I' : ['Nr_FF', 'kr_step', 'ko_step', 'Nr_max', 'Nthreads', 'store_cummulative_result'],
+    'R' : ['distance_FF', 'rmax_FF'],
+    'S' : ['XUV_table_type_dispersion', 'XUV_table_type_absorption'],
+    'R-array': ['Harmonic_range']}
+
---
+++ shared_python/inputs_transformer.py
@@ -0,0 +1,105 @@
+# Add standardised transformed inputs into hdf5-archive
+import MMA_administration as MMA
+import mynumerics as mn
+
+def add_variables2hdf5(f,
+                    global_input_names_to_jupyter_variables,
+                    CUPRAD_names_to_jupyter_variables,
+                    CTDSE_names_to_jupyter_variables,
+                    CTDSE_outputs_to_jupyter_names,
+                    list_of_CTDSE_outputs,
+                    Hankel_names_to_jupyter_variables):
+
+    
+    # global inputs
+    global_inputs = f.create_group(MMA.paths['global_inputs'])
+    for dset_name, (value, unit) in global_input_names_to_jupyter_variables.items():
+        mn.adddataset(global_inputs,dset_name,value,unit)
+    
+    
+    # CUPRAD
+    CUPRAD_inps = f.create_group(MMA.paths['CUPRAD_inputs'])
+    for dset_name, (value, unit) in CUPRAD_names_to_jupyter_variables.items():
+        mn.adddataset(CUPRAD_inps,dset_name,value,unit)
+
+
+    # CTDSE
+    # regular inputs
+    CTDSE_inps = f.create_group(MMA.paths['CTDSE_inputs'])
+    for dset_name, (value, unit) in CTDSE_names_to_jupyter_variables.items():
+        mn.adddataset(CTDSE_inps,dset_name,value,unit)
+
+    # CTDSE prints
+    mn.adddataset(CTDSE_inps,'print_GS',1.,'[-]')                        
+    for CTDSE_name, jupyter_name in CTDSE_outputs_to_jupyter_names.items():
+        mn.adddataset(CTDSE_inps,CTDSE_name,int(jupyter_name in list_of_CTDSE_outputs),'[-]')
+
+    # obsolete inputs: to be removed before the release
+    for dset_name in ('print_F_Efield_M2','print_F_Source_Term_M2','InterpByDTorNT'):
+        mn.adddataset(CTDSE_inps,dset_name,0,'[-]')
+    mn.adddataset(CTDSE_inps,'Ntinterp',1,'[-]')
+
+
+    # Hankel
+    Hankel_inps = f.create_group(MMA.paths['Hankel_inputs'])
+    for dset_name, (value, unit) in Hankel_names_to_jupyter_variables.items():
+        mn.adddataset(Hankel_inps,dset_name,value,unit)
+
+
+def line_creator(name_dict, type_dict):
+    content = ''
+    for dset_name, (value, unit) in name_dict.items():
+        if dset_name in type_dict['I']:
+            content += dset_name + '\t' + str(value) + '\t' + 'I' + '\t' + unit + '\n'
+        elif dset_name in type_dict['R']:
+            content += dset_name + '\t' + str(value) + '\t' + 'R' + '\t' + unit + '\n'
+        elif dset_name in type_dict['S']:
+            content += dset_name + '\t' + value.decode() + '\t' + 'S' + '\t' + unit + '\n'
+        elif dset_name in type_dict['R-array']:
+            content += '$array' + '\t'  + dset_name + '\t' + 'R' + '\t' + unit + '\t' +\
+                       ' '.join([str(foo) for foo in value]) +'\n'
+        else:
+            raise KeyError('The variable is not in the respective list (in type_dict).')
+
+    return content
+
+# transform to text formatted for universal inputs
+def variables2text(global_input_names_to_jupyter_variables,
+                   CUPRAD_names_to_jupyter_variables,
+                   CTDSE_names_to_jupyter_variables,
+                   CTDSE_outputs_to_jupyter_names,
+                   list_of_CTDSE_outputs,
+                   Hankel_names_to_jupyter_variables):
+
+    # global inputs
+    content = '$change_group'+'\t'+ MMA.paths['global_inputs'] +'\n\n'
+    content += line_creator(global_input_names_to_jupyter_variables, MMA.global_variable_type_lists)
+
+    # CUPRAD inputs
+    content += '\n\n' + '$change_group'+'\t'+ MMA.paths['CUPRAD_inputs'] +'\n\n'
+    content += line_creator(CUPRAD_names_to_jupyter_variables, MMA.CUPRAD_variable_type_lists)
+
+    # CTDSE inputs
+    content += '\n\n' + '$change_group'+'\t'+ MMA.paths['CTDSE_inputs'] +'\n\n'
+    content += line_creator(CTDSE_names_to_jupyter_variables, MMA.CTDSE_variable_type_lists)
+
+    # CTDSE prints
+    content += 'print_GS' + '\t' + '1' + '\t' + 'I' + '\t' + '[-]' + '\n'
+    for CTDSE_name, jupyter_name in CTDSE_outputs_to_jupyter_names.items():
+        content += CTDSE_name + '\t' + str(int(jupyter_name in list_of_CTDSE_outputs)) + '\t' +\
+                   'I' + '\t' + '[-]' + '\n'
+        # mn.adddataset(CTDSE_inps,CTDSE_name,int(jupyter_name in list_of_CTDSE_outputs),'[-]')
+
+
+    # Hankel inputs
+    content += '\n\n' + '$change_group'+'\t'+ MMA.paths['Hankel_inputs'] +'\n\n'
+    content += line_creator(Hankel_names_to_jupyter_variables, MMA.Hankel_variable_type_lists)
+
+    # for dset_name, (value, unit) in global_input_names_to_jupyter_variables.items():
+    #     content += dset_name + '\t' + str(value) + '\t' + unit + '\n'
+
+    # content += '\n\n' + '$change_group'+'\t'+ MMA.paths['CUPRAD_inputs'] +'\n\n'
+    # for dset_name, (value, unit) in CUPRAD_names_to_jupyter_variables.items():
+    #      content += dset_name + '\t' + str(value) + '\t' + unit + '\n'
+
+    return content
