74262b75ce0da54424d2473a85ffb92f991a4546
Jan Vabek <vabekjan@gmail.com>
2024-08-19 16:35:45 +0200

    CTDSE: marge into the main archive

--- 1DTDSE/post_processing/merge.py
+++ 1DTDSE/post_processing/merge.py
@@ -31,7 +31,7 @@ arguments = sys.argv
 if not(os.path.isfile('msg.tmp')): raise FileNotFoundError("'msg.tmp' required to pass the name off the archive.")
 with open('msg.tmp') as f: outfname = f.readline().rstrip()  # "results_merged.h5"
 
-outfname = 'merge_test.h5'
+# outfname = 'merge_test.h5'
 
 h5path = MMA.paths['CTDSE_outputs']
 
