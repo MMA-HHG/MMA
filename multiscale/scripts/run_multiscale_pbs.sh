fd45ce64b118d83238461b091feec9cf7aacb995
Jan Vabek <vabekjan@gmail.com>
2025-02-05 11:49:55 +0100

    pbs: attempt to mirror pbs into slurm

--- multiscale/scripts/run_multiscale_pbs.sh
+++ multiscale/scripts/run_multiscale_pbs.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Submit the pre-processor
-JOB1=$(sbatch --parsable $MULTISCALE_SCRIPTS/slurm/CUPRAD_pre_processor.slurm)
+# Submit the first job
+JOB1=$(qsub $MULTISCALE_SCRIPTS/pbs/CUPRAD_pre_processor.pbs)
 
-# Submit the main job when the pre-processor is finished
-JOB2=$(sbatch --parsable --dependency=afterok:$JOB1 $MULTISCALE_SCRIPTS/slurm/CUPRAD.slurm)
+# Submit the main job when the first job is finished (depend=afterok)
+JOB2=$(qsub -W depend=afterok:$JOB1 $MULTISCALE_SCRIPTS/pbs/CUPRAD.pbs)
 
-JOB3=$(sbatch --parsable --dependency=afterok:$JOB2 $MULTISCALE_SCRIPTS/slurm/CTDSE_prepare_MPI.slurm)
+JOB3=$(qsub -W depend=afterok:$JOB2 $MULTISCALE_SCRIPTS/pbs/CTDSE_prepare_MPI.pbs)
 
-# Run TDSE
-JOB4=$(sbatch --parsable --dependency=afterok:$JOB3 --ntasks=1800 $MULTISCALE_SCRIPTS/slurm/MPI_CTDSE.slurm)
+# Include whatever resources you need either in the PBS script itself or via command-line options
+JOB4=$(qsub -W depend=afterok:$JOB3 \
+            -l select=1:ncpus=1800 \
+            $MULTISCALE_SCRIPTS/pbs/MPI_CTDSE.pbs)
 
-# Collect & merge data
-JOB5=$(sbatch --parsable --dependency=afterok:$JOB4 $MULTISCALE_SCRIPTS/slurm/CTDSE_merge_hdf5.slurm)
+JOB5=$(qsub -W depend=afterok:$JOB4 $MULTISCALE_SCRIPTS/pbs/CTDSE_merge_hdf5.pbs)
 
-# Run Hankel
-JOBH1=$(sbatch --dependency=afterok:$JOB5 $MULTISCALE_SCRIPTS/slurm/Hankel_multiprocessing.slurm)
+# Run Hankel code after the merge completes
+JOBH1=$(qsub -W depend=afterok:$JOB5 $MULTISCALE_SCRIPTS/pbs/Hankel_multiprocessing.pbs)
