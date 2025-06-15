a5e42b7777c5db7597ff074975a11f23520e1eba
Jan Vábek <vabekjan@fjfi.cvut.cz>
2025-06-15 19:45:34 +0200

    HPC's: Karolina: adjust slurm

--- multiscale/scripts/run_multiscale_KAROLINA.sh
+++ multiscale/scripts/run_multiscale_KAROLINA.sh
@@ -9,12 +9,12 @@ JOB2=$(sbatch --parsable --dependency=afterok:$JOB1 $MULTISCALE_SCRIPTS/slurm/KA
 JOB3=$(sbatch --parsable --dependency=afterok:$JOB2 $MULTISCALE_SCRIPTS/slurm/KAROLINA/CTDSE_prepare_MPI.slurm)
 
 # Run TDSE
-JOB4=$(sbatch --parsable --dependency=afterok:$JOB3 --nodes=2 $MULTISCALE_SCRIPTS/slurm/KAROLINA/MPI_CTDSE.slurm)
+JOB4=$(sbatch --parsable --dependency=afterok:$JOB3 $MULTISCALE_SCRIPTS/slurm/KAROLINA/MPI_CTDSE.slurm)
 
 # Collect & merge data
-JOB5=$(sbatch --parsable --dependency=afterok:$JOB4 $MULTISCALE_SCRIPTS/slurm/KAROLINA/CTDSE_merge_hdf5.slurm)
+# JOB5=$(sbatch --parsable --dependency=afterok:$JOB4 $MULTISCALE_SCRIPTS/slurm/KAROLINA/CTDSE_merge_hdf5.slurm)
 
 # JOB6=$(sbatch --dependency=afterok:$JOB5 $TDSE_1D_SCRIPTS/remove_temp.slurm)
 
 
-JOBH1=$(sbatch --dependency=afterok:$JOB5 $MULTISCALE_SCRIPTS/slurm/KAROLINA/Hankel_multiprocessing.slurm)
+# JOBH1=$(sbatch --dependency=afterok:$JOB5 $MULTISCALE_SCRIPTS/slurm/KAROLINA/Hankel_multiprocessing.slurm)
